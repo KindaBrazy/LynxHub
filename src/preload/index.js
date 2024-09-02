10cd350cf5f185fc2955fa55c0245faebd050c39
kindabrazy <kindofbrazy@gmail.com>
2024-09-02 19:15:31 +0330

    Expose os.platform to renderer
    Disable pre-release check autoUpdater
    
    Update dependencies to latest versions (Note:Electron V31.4.0 -> V32.0.1, node-pty V1.0.0 -> V1.1.0-beta21)

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,12 +1,16 @@
+import os from 'node:os';
+
 import {electronAPI} from '@electron-toolkit/preload';
 import {contextBridge} from 'electron';
 
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI);
+    contextBridge.exposeInMainWorld('osPlatform', os.platform());
   } catch (error) {
     console.error(error);
   }
 } else {
   window.electron = electronAPI;
+  window.osPlatform = os.platform();
 }
