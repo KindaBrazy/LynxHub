a0046923022c542c156d42156b6706db845b1a6f
kindabrazy <kindofbrazy@gmail.com>
2024-09-30 20:44:07 +0330

    chore(ui): improve navigation bar style and visibility

--- src/renderer/src/App/Components/NavBar/NavBar.tsx
+++ src/renderer/src/App/Components/NavBar/NavBar.tsx
@@ -5,15 +5,17 @@ import {ContentPagesButtons, SettingsPagesButtons} from './NavButtons';
 const CONTAINER_WIDTH = 'w-[5.5rem]';
 
 const NAV_BAR_STYLE = {
-  bgColor: 'dark:bg-black/10 bg-white/50',
+  bgColor: 'dark:bg-LynxRaisinBlack bg-white',
   gapY: 'gap-y-1',
   paddingY: 'py-3',
   shadow: 'shadow-sm',
   width: 'w-14',
 };
 
-const COMMON_STYLES = `${NAV_BAR_STYLE.width} ${NAV_BAR_STYLE.bgColor} ${NAV_BAR_STYLE.shadow} ${NAV_BAR_STYLE.paddingY}
-   ${NAV_BAR_STYLE.gapY} flex relative min-h-16 flex-col flex-nowrap overflow-y-scroll rounded-full scrollbar-hide`;
+const COMMON_STYLES =
+  `${NAV_BAR_STYLE.width} ${NAV_BAR_STYLE.bgColor} ${NAV_BAR_STYLE.shadow} ${NAV_BAR_STYLE.paddingY}` +
+  ` ${NAV_BAR_STYLE.gapY} flex relative min-h-16 flex-col flex-nowrap overflow-y-scroll rounded-full ` +
+  `scrollbar-hide drop-shadow-md border border-foreground/5`;
 
 /** Navigation bar containing two sections: Contents and Settings */
 const NavBar = memo(() => {
@@ -22,7 +24,7 @@ const NavBar = memo(() => {
       <div className={`${COMMON_STYLES} max-h-[56%]`}>
         <ContentPagesButtons />
       </div>
-      <div className={`${COMMON_STYLES} sm:max-h-[30%] lg:max-h-[30%] xl:max-h-[35%]`}>
+      <div className={`${COMMON_STYLES} sm:max-h-[35%] lg:max-h-[35%] xl:max-h-[40%]`}>
         <SettingsPagesButtons />
       </div>
     </div>
