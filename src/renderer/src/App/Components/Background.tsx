564251db57f16b6b0ed3f73efa3d9d1ceec9da8d
kindabrazy <kindofbrazy@gmail.com>
2024-10-01 17:10:56 +0330

    chore(ui): add soft teal background for dark mode

--- src/renderer/src/App/Components/Background.tsx
+++ src/renderer/src/App/Components/Background.tsx
@@ -1,7 +1,11 @@
 import {memo} from 'react';
 
 const Background = memo(() => {
-  return <div className="absolute inset-0 bg-GradientLight dark:bg-GradientDark" />;
+  return (
+    <div className="absolute inset-0 bg-GradientLight dark:bg-GradientDark">
+      <div className={'absolute inset-0 blur-[51px] dark:bg-cyan-700/10'} />
+    </div>
+  );
 });
 
 export default Background;
