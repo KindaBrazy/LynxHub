776e4b1817dd654e9c2e938a34640319e70a69ae
kindabrazy <kindofbrazy@gmail.com>
2024-10-04 13:26:12 +0330

    chore(security): update Content-Security-Policy

--- src/renderer/src/App/Utils/SetHtmlAttributes.tsx
+++ src/renderer/src/App/Utils/SetHtmlAttributes.tsx
@@ -2,18 +2,6 @@ import {useEffect} from 'react';
 
 import {APP_NAME} from '../../../../cross/CrossConstants';
 import {useAppState} from '../Redux/App/AppReducer';
-import rendererIpc from '../RendererIpc';
-
-const CONNECT_SRC =
-  'https://github.com/ ' +
-  'https://api.github.com/ ' +
-  'https://gitlab.com/api/ ' +
-  'https://gitlab.com/uploads/ ' +
-  'https://secure.gravatar.com/avatar/ ' +
-  'https://*.githubusercontent.com/ ' +
-  'https://image.civitai.com/ ' +
-  'https://*.patreonusercontent.com/ ' +
-  `${import.meta.env.DEV && 'ws://localhost:5173/'}`;
 
 /** HTML attributes and document title */
 export default function useHtmlAttributes() {
@@ -26,36 +14,5 @@ export default function useHtmlAttributes() {
 
     // Set the document title
     document.title = APP_NAME;
-
-    async function setPolicy() {
-      const moduleData = await rendererIpc.module.getModulesData();
-
-      const port = new URL(moduleData[0]).port;
-
-      // Set Content Security Policy (CSP)
-      const csp = [
-        "default-src 'self';",
-        "script-src 'self';",
-        `script-src-elem 'self' http://localhost:${port};`,
-        "style-src 'self' 'unsafe-inline';",
-        `img-src 'self' data: ${CONNECT_SRC};`,
-        'frame-src *;',
-        `connect-src ${CONNECT_SRC};`,
-      ].join(' ');
-
-      const metaCSP = document.createElement('meta');
-      metaCSP.httpEquiv = 'Content-Security-Policy';
-      metaCSP.content = csp;
-
-      // Remove any existing CSP and add the new one
-      const existingMetaCSP = document.querySelector('meta[http-equiv="Content-Security-Policy"]');
-      if (existingMetaCSP) {
-        existingMetaCSP.parentNode?.removeChild(existingMetaCSP);
-      }
-
-      document.head.appendChild(metaCSP);
-    }
-
-    setPolicy();
   }, [darkMode]);
 }
