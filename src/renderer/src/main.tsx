fbd5598b70525dbe2724184992ad26082bdbb96f
kindabrazy <kindofbrazy@gmail.com>
2024-10-04 13:28:07 +0330

    feat(logging): save renderer logs to log file

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,11 +3,14 @@ import '@xterm/xterm/css/xterm.css';
 import 'overlayscrollbars/overlayscrollbars.css';
 import '@mantine/core/styles.css';
 
+import log from 'electron-log/renderer';
 import {createRoot} from 'react-dom/client';
 import {RouterProvider} from 'react-router-dom';
 
 import {initRouter} from './App/AppRouter';
 
 const router = await initRouter();
+
+Object.assign(console, log.functions);
 
 createRoot(document.getElementById('root') as HTMLElement).render(<RouterProvider router={router} />);
