97ca987f014a66c2eafa1e3a4d99008515b9cc2e
kindabrazy <kindofbrazy@gmail.com>
2024-10-22 12:56:04 +0330

    feat(extensions): add development configuration and basic entry point for renderer process

---
+++ electron.vite.extension.config.ts
@@ -0,0 +1,45 @@
+import federation from '@originjs/vite-plugin-federation';
+import react from '@vitejs/plugin-react';
+import {defineConfig, externalizeDepsPlugin} from 'electron-vite';
+import {resolve} from 'path';
+
+// import packageJson from './package.json';
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: resolve('src/main/extension/lynxExtension.ts'),
+        output: {entryFileNames: 'mainEntry.mjs'},
+      },
+    },
+  },
+  renderer: {
+    plugins: [
+      react(),
+      federation({
+        name: 'extension',
+        filename: 'rendererEntry.mjs',
+        exposes: {
+          Extension: './src/renderer/extension/Extension.tsx',
+        },
+        shared: ['react', 'react-dom', 'react-redux'],
+      }),
+    ],
+    build: {
+      rollupOptions: {
+        input: 'src/renderer/extension/index.html',
+        // external: Object.keys(packageJson.devDependencies),
+        treeshake: {moduleSideEffects: false},
+      },
+      assetsDir: '',
+      minify: false,
+      target: 'esnext',
+      cssCodeSplit: false,
+      modulePreload: false,
+    },
+
+    publicDir: resolve(__dirname, 'src/renderer/extension/public'),
+  },
+});
