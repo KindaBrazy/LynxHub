02f7acfef53fe6ba522efc8734c4a56468143d00
kindabrazy <kindofbrazy@gmail.com>
2024-10-22 11:58:52 +0330

    chore(tailwind): update Tailwind export configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 import {nextui} from '@nextui-org/react';
 
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './src/renderer/index.html',
     './src/renderer/initializer.html',
