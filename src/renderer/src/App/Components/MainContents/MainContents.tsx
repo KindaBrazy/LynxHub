f777b1d6135cdf29f94db89c2f66ce7c0e23edbb
kindabrazy <kindofbrazy@gmail.com>
2024-10-23 15:48:21 +0330

    feat(extensions): props for extension components refactor: separate MainContent into components for better readability

---
+++ src/renderer/src/App/Components/MainContents/MainContents.tsx
@@ -0,0 +1,20 @@
+import {memo} from 'react';
+
+import NavBar from '../NavBar/NavBar';
+import ContentLoading from './ContentLoading';
+import StatusBar from './StatusBar';
+
+/** Main app contents */
+const MainContents = memo(() => {
+  return (
+    <div className="absolute inset-0 top-10 flex flex-col transition duration-300">
+      <div className="relative flex size-full flex-row overflow-hidden">
+        <NavBar />
+        <ContentLoading />
+      </div>
+      <StatusBar />
+    </div>
+  );
+});
+
+export default MainContents;
