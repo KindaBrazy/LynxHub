f4039f2d94f83a8074634158992b09e3399f3a42
kindabrazy <kindofbrazy@gmail.com>
2024-10-26 12:04:30 +0330

    feat(environment): load main process extensions in both dev and prod environments

--- src/main/Managements/Plugin/ExtensionManager.ts
+++ src/main/Managements/Plugin/ExtensionManager.ts
@@ -3,6 +3,7 @@ import path from 'node:path';
 import compact from 'lodash/compact';
 
 import {ExtensionsInfo, MainExtensions} from '../../../cross/CrossTypes';
+import {isDev} from '../../../cross/CrossUtils';
 import {extensionsChannels} from '../../../cross/IpcChannelAndTypes';
 import {getAppDirectory} from '../AppDataManager';
 import {BasePluginManager} from './BasePluginManager';
@@ -25,20 +26,27 @@ export default class ExtensionManager extends BasePluginManager<ExtensionsInfo, 
   }
 
   protected async importPlugins(extensionFolders: string[]) {
-    const importedExtensions = await Promise.all(
-      extensionFolders.map(async extensionPath => {
-        const id = this.installedPluginInfo.find(plugin => plugin.dir === extensionPath)?.info.id;
-        if (!id) return;
-        return {
-          id,
-          methods: await import(`file://${path.join(extensionPath, 'scripts', 'main', 'mainEntry.mjs')}`),
-        };
-      }),
-    );
+    if (isDev()) {
+      const json = await import('../../extension/lynxExtension.json');
+      const id = json.id;
+      const methods = await import('../../extension/lynxExtension');
+      this.mainMethods.push({id, methods});
+    } else {
+      const importedExtensions = await Promise.all(
+        extensionFolders.map(async extensionPath => {
+          const id = this.installedPluginInfo.find(plugin => plugin.dir === extensionPath)?.info.id;
+          if (!id) return;
+          return {
+            id,
+            methods: await import(`file://${path.join(extensionPath, 'scripts', 'main', 'mainEntry.mjs')}`),
+          };
+        }),
+      );
 
-    compact(importedExtensions).forEach((importedExtension: MainExtensions) => {
-      this.mainMethods.push({id: importedExtension.id, methods: importedExtension.methods});
-    });
+      compact(importedExtensions).forEach((importedExtension: MainExtensions) => {
+        this.mainMethods.push({id: importedExtension.id, methods: importedExtension.methods});
+      });
+    }
   }
 
   public listenForChannels() {
