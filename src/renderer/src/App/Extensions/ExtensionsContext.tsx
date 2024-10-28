98a9a3708da815a3ef50e40eb852b6a929bd1976
kindabrazy <kindofbrazy@gmail.com>
2024-10-28 15:07:55 +0330

    feat(extensions): add support for TitleBar customization through extensions

--- src/renderer/src/App/Extensions/ExtensionsContext.tsx
+++ src/renderer/src/App/Extensions/ExtensionsContext.tsx
@@ -4,13 +4,14 @@ import {createContext, ReactNode, useContext, useEffect, useMemo, useState} from
 import {isDev} from '../../../../cross/CrossUtils';
 import {ElementComp} from '../../../../cross/ExtensionTypes';
 import rendererIpc from '../RendererIpc';
-import {loadStatusBar} from './ExtensionLoader';
-import {ExtensionAppBackground, ExtensionImport, ExtensionStatusBar} from './ExtensionTypes';
+import {loadStatusBar, loadTitleBar} from './ExtensionLoader';
+import {ExtensionAppBackground, ExtensionImport, ExtensionStatusBar, ExtensionTitleBar} from './ExtensionTypes';
 import {getRemote, setRemote} from './Vite-Federation';
 
 type ExtensionContextData = {
   loadingExtensions: boolean;
   statusBar: ExtensionStatusBar;
+  titleBar: ExtensionTitleBar;
   background: ExtensionAppBackground;
   customHooks: ElementComp[];
 };
@@ -18,6 +19,7 @@ type ExtensionContextData = {
 const ExtensionContext = createContext<ExtensionContextData>({
   loadingExtensions: false,
   statusBar: undefined,
+  titleBar: undefined,
   background: undefined,
   customHooks: [],
 });
@@ -26,14 +28,16 @@ export default function ExtensionsProvider({children}: {children: ReactNode}) {
   const [loadingExtensions, setLoadingExtensions] = useState<boolean>(false);
 
   const [statusBar, setStatusBar] = useState<ExtensionStatusBar>(undefined);
+  const [titleBar, setTitleBar] = useState<ExtensionTitleBar>(undefined);
   const [background, setBackground] = useState<ExtensionAppBackground>(undefined);
   const [customHooks, setCustomHooks] = useState<ElementComp[]>([]);
 
   useEffect(() => {
     const loadExtensions = async () => {
       setStatusBar(undefined);
+      setTitleBar(undefined);
       setCustomHooks([]);
-      let importedExtensions: ExtensionImport[] = [];
+      let importedExtensions: ExtensionImport[];
       if (isDev()) {
         const extension = await import('../../../extension/Extension');
         importedExtensions = [extension];
@@ -60,10 +64,12 @@ export default function ExtensionsProvider({children}: {children: ReactNode}) {
       }
 
       const StatusBars = compact(importedExtensions.map(ext => ext.StatusBar));
+      const TitleBar = compact(importedExtensions.map(ext => ext.TitleBar));
       const Background = compact(importedExtensions.map(ext => ext.Background));
       const CustomHooks = compact(importedExtensions.map(ext => ext.CustomHook));
 
       if (!isEmpty(StatusBars)) loadStatusBar(setStatusBar, StatusBars);
+      if (!isEmpty(TitleBar)) loadTitleBar(setTitleBar, TitleBar);
       if (!isEmpty(Background)) setBackground(Background[0]);
       if (!isEmpty(CustomHooks)) setCustomHooks(CustomHooks);
 
@@ -74,8 +80,8 @@ export default function ExtensionsProvider({children}: {children: ReactNode}) {
   }, []);
 
   const contextValue: ExtensionContextData = useMemo(() => {
-    return {loadingExtensions, statusBar, background, customHooks};
-  }, [loadingExtensions, statusBar, background, customHooks]);
+    return {loadingExtensions, statusBar, titleBar, background, customHooks};
+  }, [loadingExtensions, statusBar, titleBar, background, customHooks]);
 
   return <ExtensionContext.Provider value={contextValue}>{children}</ExtensionContext.Provider>;
 }
