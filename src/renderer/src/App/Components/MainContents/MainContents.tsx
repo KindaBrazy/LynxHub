4a4f53cabeef6143229d1cab4bf29a89127afd66
kindabrazy <kindofbrazy@gmail.com>
2024-10-28 11:00:11 +0330

    style(MainContent): add !important to top-10 CSS property

--- src/renderer/src/App/Components/MainContents/MainContents.tsx
+++ src/renderer/src/App/Components/MainContents/MainContents.tsx
@@ -7,7 +7,7 @@ import StatusBar from './StatusBar';
 /** Main app contents */
 const MainContents = memo(() => {
   return (
-    <div className="absolute inset-0 top-10 flex flex-col transition duration-300">
+    <div className="absolute inset-0 !top-10 flex flex-col transition duration-300">
       <div className="relative flex size-full flex-row overflow-hidden">
         <NavBar />
         <ContentLoading />
