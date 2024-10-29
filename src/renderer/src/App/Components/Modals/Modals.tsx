9d72c02aaff15dd4c99124306c33e7ca26e2702e
kindabrazy <kindofbrazy@gmail.com>
2024-10-29 13:24:24 +0330

    feat(extensions): add support for Modals integration through extensions

--- src/renderer/src/App/Components/Modals/Modals.tsx
+++ src/renderer/src/App/Components/Modals/Modals.tsx
@@ -1,5 +1,6 @@
 import {memo} from 'react';
 
+import {useExtensions} from '../../Extensions/ExtensionsContext';
 import CardExtensions from './CardExtensions/CardExtensions';
 import CardInfoModal from './CardInfo/CardInfo-Modal';
 import InstallModal from './Install/InstallModal';
@@ -11,17 +12,19 @@ import UpdatingNotification from './UpdatingCard/UpdatingNotification';
 import WarningModal from './Warning/WarningModal';
 
 const Modals = memo(() => {
+  const {modals} = useExtensions();
+
   return (
     <>
-      <WarningModal />
-      <UninstallCard />
-      <InstallModal />
-      <InstallUIModal />
-      <CardInfoModal />
-      <UpdatingNotification />
-      <CardExtensions />
-      <LaunchConfig />
-      <UpdateApp />
+      {modals?.WarningModal ? <modals.WarningModal /> : <WarningModal />}
+      {modals?.UninstallCard ? <modals.UninstallCard /> : <UninstallCard />}
+      {modals?.InstallModal ? <modals.InstallModal /> : <InstallModal />}
+      {modals?.InstallUIModal ? <modals.InstallUIModal /> : <InstallUIModal />}
+      {modals?.CardInfoModal ? <modals.CardInfoModal /> : <CardInfoModal />}
+      {modals?.UpdatingNotification ? <modals.UpdatingNotification /> : <UpdatingNotification />}
+      {modals?.CardExtensions ? <modals.CardExtensions /> : <CardExtensions />}
+      {modals?.LaunchConfig ? <modals.LaunchConfig /> : <LaunchConfig />}
+      {modals?.UpdateApp ? <modals.UpdateApp /> : <UpdateApp />}
     </>
   );
 });
