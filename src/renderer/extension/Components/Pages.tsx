107761439c67abb79cb7c4a03c0d1a9596252fb5
kindabrazy <kindofbrazy@gmail.com>
2024-10-30 18:54:50 +0330

    refactor(extensions_renderer): use Page component in extension router page example

--- src/renderer/extension/Components/Pages.tsx
+++ src/renderer/extension/Components/Pages.tsx
@@ -1,9 +1,10 @@
 import {ExtensionPages} from '../../../cross/ExtensionTypes';
+import Page from '../../src/App/Components/Pages/Page';
 
 const Pages: ExtensionPages = {};
 
 function HomePage() {
-  return <div className="flex size-full items-center justify-center">Home Page Baby!</div>;
+  return <Page className="flex size-full items-center justify-center text-4xl text-pink-600">Home Page Baby!</Page>;
 }
 
 function ImageGenerationPage() {
