040e467ac11225d2f60caa2f4e560400320ea250
kindabrazy <kindofbrazy@gmail.com>
2024-10-30 12:46:52 +0330

    feat(extensions_renderer): add support for Running AI View customization through extensions

--- src/renderer/src/App/Components/MainContents/AppPages.tsx
+++ src/renderer/src/App/Components/MainContents/AppPages.tsx
@@ -1,9 +1,12 @@
 import {Outlet} from 'react-router-dom';
 
+import {useExtensions} from '../../Extensions/ExtensionsContext';
 import {useCardsState} from '../../Redux/AI/CardsReducer';
 import RunningCardView from '../RunningCardView/RunningCardView';
 
 export default function AppPages() {
   const {isRunning} = useCardsState('runningCard');
-  return isRunning ? <RunningCardView /> : <Outlet />;
+  const {runningAI} = useExtensions();
+
+  return isRunning ? runningAI?.Container ? <runningAI.Container /> : <RunningCardView /> : <Outlet />;
 }
