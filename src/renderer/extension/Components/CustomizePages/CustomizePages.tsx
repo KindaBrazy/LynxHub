357668da922e0af32cf8517bb717d2f0162fca4c
kindabrazy <kindofbrazy@gmail.com>
2024-10-31 18:30:33 +0330

    chore(extensions_renderer): add home customization example to extension sample

---
+++ src/renderer/extension/Components/CustomizePages/CustomizePages.tsx
@@ -0,0 +1,8 @@
+import {CustomizePagesComponent} from '../../../../cross/ExtensionTypes';
+import CustomizeHomePage from './HomePage';
+
+const CustomizePages: CustomizePagesComponent = {
+  CustomizeHomePage,
+};
+
+export default CustomizePages;
