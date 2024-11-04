2e88bff0cbc7f072f52450ab24489fb1cfd0f027
kindabrazy <kindofbrazy@gmail.com>
2024-11-04 16:49:14 +0330

    feat(extensions_renderer): add support for adding elements to the image, audio and text generation page through extensions

--- src/renderer/src/App/Components/Pages/ContentPages/AudioGenerationPage.tsx
+++ src/renderer/src/App/Components/Pages/ContentPages/AudioGenerationPage.tsx
@@ -1,5 +1,7 @@
 import {ScrollShadow} from '@nextui-org/react';
+import {useState} from 'react';
 
+import {extensionsData} from '../../../Extensions/ExtensionLoader';
 import {GetComponentsByPath} from '../../Cards/Cards';
 import CardContainer from '../CardContainer';
 import Page from '../Page';
@@ -9,13 +11,21 @@ export const audioGenElementId: string = 'audioGenElement';
 
 // Generating audios with AI
 export default function AudioGenerationPage() {
+  const [customizePage] = useState(extensionsData.customizePages.audio);
+
   return (
     <Page className="pt-6" id={audioGenElementId}>
+      {customizePage.add.top && customizePage.add.top.map((Top, index) => <Top key={index} />)}
       <ScrollShadow size={20} className="size-full overflow-y-scroll pb-4 scrollbar-hide">
+        {customizePage.add.scrollTop &&
+          customizePage.add.scrollTop.map((ScrollTop, index) => <ScrollTop key={index} />)}
         <CardContainer icon="AudioGeneration" extraClassNames="mr-3" title="Audio Generation">
           <GetComponentsByPath routePath={audioGenRoutePath} />
         </CardContainer>
+        {customizePage.add.scrollBottom &&
+          customizePage.add.scrollBottom.map((ScrollBottom, index) => <ScrollBottom key={index} />)}
       </ScrollShadow>
+      {customizePage.add.bottom && customizePage.add.bottom.map((Bottom, index) => <Bottom key={index} />)}
     </Page>
   );
 }
--- src/renderer/src/App/Components/Pages/ContentPages/ImageGenerationPage.tsx
+++ src/renderer/src/App/Components/Pages/ContentPages/ImageGenerationPage.tsx
@@ -1,5 +1,7 @@
 import {ScrollShadow} from '@nextui-org/react';
+import {useState} from 'react';
 
+import {extensionsData} from '../../../Extensions/ExtensionLoader';
 import {GetComponentsByPath} from '../../Cards/Cards';
 import CardContainer from '../CardContainer';
 import Page from '../Page';
@@ -9,13 +11,20 @@ export const imageGenElementId: string = 'imageGenElement';
 
 // Generating images with AI
 export default function ImageGenerationPage() {
+  const [customizePage] = useState(extensionsData.customizePages.image);
   return (
     <Page className="pt-6" id={imageGenElementId}>
+      {customizePage.add.top && customizePage.add.top.map((Top, index) => <Top key={index} />)}
       <ScrollShadow size={20} className="size-full overflow-y-scroll pb-4 scrollbar-hide">
+        {customizePage.add.scrollTop &&
+          customizePage.add.scrollTop.map((ScrollTop, index) => <ScrollTop key={index} />)}
         <CardContainer icon="ImageGeneration" extraClassNames="mr-3" title="Image Generation">
           <GetComponentsByPath routePath={imageGenRoutePath} />
         </CardContainer>
+        {customizePage.add.scrollBottom &&
+          customizePage.add.scrollBottom.map((ScrollBottom, index) => <ScrollBottom key={index} />)}
       </ScrollShadow>
+      {customizePage.add.bottom && customizePage.add.bottom.map((Bottom, index) => <Bottom key={index} />)}
     </Page>
   );
 }
