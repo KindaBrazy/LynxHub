45a70073e650f0d2adfb4b178b45db5e8c425b96
kindabrazy <kindofbrazy@gmail.com>
2024-11-05 12:55:28 +0330

    chore(extensions_renderer): add example and usage of Redux reducer in extension sample

--- src/renderer/extension/Extension.tsx
+++ src/renderer/extension/Extension.tsx
@@ -9,12 +9,15 @@ import {
   HomePage_ReplaceCategories,
   HomePage_Top,
   HomePage_TopScroll,
+  ReducerTester,
   routePage,
   StatusBarEnd,
 } from './Components';
+import extensionReducer from './reducer';
 
 export function InitialExtensions(lynxAPI: ExtensionRendererApi) {
   return;
+
   lynxAPI.statusBar.addEnd(StatusBarEnd);
   lynxAPI.router.add(routePage);
   lynxAPI.navBar.addButton.contentBar(AddContentButton);
@@ -24,4 +27,7 @@ export function InitialExtensions(lynxAPI: ExtensionRendererApi) {
   lynxAPI.customizePages.home.replace.categories(HomePage_ReplaceCategories);
   lynxAPI.customizePages.home.add.top(HomePage_Top);
   lynxAPI.customizePages.home.add.scrollBottom(HomePage_TopScroll);
+
+  lynxAPI.customizePages.audio.add.scrollBottom(ReducerTester);
+  lynxAPI.addReducer([{name: 'extension', reducer: extensionReducer}]);
 }
