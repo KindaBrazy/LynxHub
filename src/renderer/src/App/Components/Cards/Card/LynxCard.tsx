53b04f0328e547f5f50c622eee56d5ed6da18597
kindabrazy <kindofbrazy@gmail.com>
2024-11-07 17:36:19 +0330

    feat(ui): add ribbon to pinned cards

--- src/renderer/src/App/Components/Cards/Card/LynxCard.tsx
+++ src/renderer/src/App/Components/Cards/Card/LynxCard.tsx
@@ -2,7 +2,11 @@ import {Card} from '@nextui-org/react';
 import {Badge} from 'antd';
 import {motion} from 'framer-motion';
 import {observer} from 'mobx-react-lite';
+import {useMemo} from 'react';
 
+import {getIconByName} from '../../../../assets/icons/SvgIconsContainer';
+import {useCardsState} from '../../../Redux/AI/CardsReducer';
+import {useAppState} from '../../../Redux/App/AppReducer';
 import {useSettingsState} from '../../../Redux/App/SettingsReducer';
 import {useUpdateAvailable} from '../../../Utils/UtilHooks';
 import {useCardData} from '../CardsDataManager';
@@ -15,6 +19,12 @@ const LynxCard = observer(() => {
   const compactMode = useSettingsState('cardsCompactMode');
   const cardsRepoInfo = useSettingsState('cardsRepoInfo');
   const updateAvailable = useUpdateAvailable(id);
+  const pinnedCards = useCardsState('pinnedCards');
+  const darkMode = useAppState('darkMode');
+
+  const isPinned = useMemo(() => {
+    return pinnedCards.includes(id);
+  }, [pinnedCards]);
 
   return (
     <Badge.Ribbon
@@ -24,21 +34,27 @@ const LynxCard = observer(() => {
       color="green"
       placement="end"
       text="Update Available">
-      <motion.div
-        layout="position"
-        exit={{scale: 0.9, opacity: 0}}
-        animate={{scale: 1, opacity: 1}}
-        initial={{scale: 0.9, opacity: 0}}>
-        <Card
-          className={
-            ` ${compactMode ? 'w-[230px]' : 'w-[277px]'} h-fit cursor-default shadow-md !transition ` +
-            ` border-1 border-foreground/10 duration-300 hover:shadow-xl dark:bg-[#323232]`
-          }>
-          <LynxCardHeader />
-          {!compactMode && cardsRepoInfo && <LynxCardBody />}
-          <LynxCardFooter />
-        </Card>
-      </motion.div>
+      <Badge.Ribbon
+        placement="start"
+        color={darkMode ? '#AA00FF' : '#d500f9'}
+        text={getIconByName('Pin', {className: 'm-1'})}
+        className={`z-10 ${isPinned ? 'block opacity-100' : 'hidden opacity-0'} transition duration-500`}>
+        <motion.div
+          layout="position"
+          exit={{scale: 0.9, opacity: 0}}
+          animate={{scale: 1, opacity: 1}}
+          initial={{scale: 0.9, opacity: 0}}>
+          <Card
+            className={
+              ` ${compactMode ? 'w-[230px]' : 'w-[277px]'} h-fit cursor-default shadow-md !transition ` +
+              ` border-1 border-foreground/10 duration-300 hover:shadow-xl dark:bg-[#323232]`
+            }>
+            <LynxCardHeader />
+            {!compactMode && cardsRepoInfo && <LynxCardBody />}
+            <LynxCardFooter />
+          </Card>
+        </motion.div>
+      </Badge.Ribbon>
     </Badge.Ribbon>
   );
 });
