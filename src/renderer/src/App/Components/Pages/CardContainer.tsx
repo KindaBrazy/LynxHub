d8c4512a74227dfff5d00bcc88436f2de99b9cf2
kindabrazy <kindofbrazy@gmail.com>
2024-11-09 18:39:07 +0330

    feat(ui): add descriptions to categories
    style(ui): make card container background slightly lighter

--- src/renderer/src/App/Components/Pages/CardContainer.tsx
+++ src/renderer/src/App/Components/Pages/CardContainer.tsx
@@ -7,23 +7,24 @@ type Props = {
   children: ReactNode;
   icon: IconNameType;
   title: string;
+  subTitle?: string;
   extraClassNames?: string;
 };
 
-export default function CardContainer({children, icon, title, extraClassNames}: Props) {
+export default function CardContainer({children, icon, title, subTitle, extraClassNames}: Props) {
   return (
     <Card
-      className={['border-2 border-foreground/5 bg-white !shadow-medium dark:bg-LynxRaisinBlack', extraClassNames].join(
-        ' ',
-      )}
       title={
-        <div className="flex w-full flex-row items-center justify-between overflow-visible">
-          {getIconByName(icon, {className: 'size-5 mr-2 hover:transition hover:duration-500 hover:opacity-80'})}
-          <span className="text-lg font-bold hover:opacity-80 hover:transition hover:duration-500">{title}</span>
-          <div />
+        <div className="flex w-full flex-col overflow-visible py-2">
+          <div className="flex flex-row items-center">
+            {getIconByName(icon, {className: 'size-6 mr-2 hover:transition hover:duration-500 hover:opacity-80'})}
+            <span className="text-lg font-bold hover:opacity-80 hover:transition hover:duration-500">{title}</span>
+          </div>
+          <span className="text-small text-foreground-500">{subTitle}</span>
         </div>
       }
-      bordered={false}>
+      bordered={false}
+      className={['border-2 border-foreground/5 bg-white !shadow-medium dark:bg-[#292929]', extraClassNames].join(' ')}>
       {children}
     </Card>
   );
