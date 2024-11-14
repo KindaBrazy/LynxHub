c5475d9bbb7f791756a1fab5bb2d454d3ee88abd
kindabrazy <kindofbrazy@gmail.com>
2024-11-14 17:47:20 +0330

    feat(ui): remove repo page button from card menu

--- src/renderer/src/App/Components/Cards/Card/Menu/UseCardMenuSections.tsx
+++ src/renderer/src/App/Components/Cards/Card/Menu/UseCardMenuSections.tsx
@@ -3,7 +3,7 @@ import {compact} from 'lodash';
 import {useMemo} from 'react';
 
 import {DropDownSectionType} from '../../../../Utils/Types';
-import {useDocPage, useInfo} from './MenuItems/CardMenu-About';
+import {useInfo} from './MenuItems/CardMenu-About';
 import {useUninstall} from './MenuItems/CardMenu-Danger';
 import {useExtensions, useLaunchConfig, usePin} from './MenuItems/CardMenu-Options';
 import {useAutoUpdate, useCheckForUpdate, useUpdate} from './MenuItems/CardMenu-Update';
@@ -17,7 +17,6 @@ const useCardMenuSections = (): DropDownSectionType[] => {
   const checkForUpdate = useCheckForUpdate();
   const autoUpdate = useAutoUpdate();
 
-  const docPage = useDocPage();
   const info = useInfo();
 
   const uninstall = useUninstall();
@@ -32,9 +31,7 @@ const useCardMenuSections = (): DropDownSectionType[] => {
     [update, checkForUpdate, autoUpdate],
   );
 
-  const aboutItems: DropdownItemProps[] = useMemo(() => compact([docPage, info]), [docPage, info]);
-
-  const dangerItems: DropdownItemProps[] = useMemo(() => compact([uninstall]), [uninstall]);
+  const extraItems: DropdownItemProps[] = useMemo(() => compact([info, uninstall]), [info, uninstall]);
 
   return [
     {
@@ -50,13 +47,8 @@ const useCardMenuSections = (): DropDownSectionType[] => {
       items: updateItems,
     },
     {
-      key: 'about',
-      title: 'About',
-      items: aboutItems,
-    },
-    {
       key: 'danger-zone',
-      items: dangerItems,
+      items: extraItems,
     },
   ];
 };
