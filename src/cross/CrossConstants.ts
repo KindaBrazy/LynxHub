f9b316b8319243bf136db34feeb32235eb4d2f54
kindabrazy <kindofbrazy@gmail.com>
2024-11-23 20:05:49 +0330

    refactor(package): remove moduleContainer and mainModule from package.json and replace with Constants

--- src/cross/CrossConstants.ts
+++ src/cross/CrossConstants.ts
@@ -7,9 +7,6 @@ export const APP_BUILD_NUMBER: number = packageJson.appDetails.buildNumber;
 export const APP_DESCRIPTION: string = packageJson.description;
 export const APP_DETAILED_DESCRIPTION: string = packageJson.appDetails.detailedDescription;
 
-export const MODULE_CONTAINER: string = packageJson.appDetails.moduleContainer;
-export const MAIN_MODULE_URL: string = packageJson.appDetails.mainModule;
-
 export const RELEASES_PAGE = `${packageJson.homepage}/releases`;
 
 export const EMAIL: string = packageJson.author.email;
@@ -25,12 +22,18 @@ export const MODULES_FOLDER_NAME = 'Modules' as const;
 export const EXTENSIONS_FOLDER_NAME = 'Extensions' as const;
 export const BINARIES_FOLDER_NAME = 'Binaries' as const;
 export const REPOSITORIES_FOLDER_NAME = 'AIWorkspaces' as const;
+
 // Static Assets
 export const APP_ICON_TRANSPARENT: string = 'AppIcon Transparent.png';
 
+// Raw GitHub Files
+export const MODULE_CONTAINER: string =
+  'https://raw.githubusercontent.com/KindaBrazy/LynxHub-Modules/compiled/container.json';
 export const WIN_RELEASE_URL: string = 'https://raw.githubusercontent.com/KindaBrazy/LynxHub/master/releases_log.json';
 export const PATRONS_DATA =
   'https://raw.githubusercontent.com/KindaBrazy/LynxHub-Patrons/refs/heads/main/data/patrons.json';
+
+export const MAIN_MODULE_URL: string = 'https://github.com/KindaBrazy/LynxHub-Module-Offline-Container';
 export const DISCORD_SERVER: string = 'https://discord.gg/e8rBzhtcnK';
 export const X_URL: string = 'https://x.com/LynxHubAI';
 export const PATREON_URL: string = 'https://www.patreon.com/LynxHub';
