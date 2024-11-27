5864d3d5e9c5f249496aed662f5142a43391dd8e
kindabrazy <kindofbrazy@gmail.com>
2024-11-27 12:39:48 +0330

    fix(extensions_renderer): resolve issue with replacing status bar container

--- src/renderer/src/App/Components/MainContents/StatusBar.tsx
+++ src/renderer/src/App/Components/MainContents/StatusBar.tsx
@@ -1,4 +1,4 @@
-import {isEmpty} from 'lodash';
+import {isEmpty, isNil} from 'lodash';
 import {memo, useMemo} from 'react';
 
 import {extensionsData} from '../../Extensions/ExtensionLoader';
@@ -6,7 +6,7 @@ import {extensionsData} from '../../Extensions/ExtensionLoader';
 const classNames = 'flex size-full items-center overflow-x-scroll scrollbar-hide';
 
 const StatusBar = () => {
-  const {addEnd, addStart, addCenter, replaceContainer} = useMemo(() => extensionsData.statusBar, []);
+  const {addEnd, addStart, addCenter, replaceContainer: ReplaceContainer} = useMemo(() => extensionsData.statusBar, []);
 
   const isEmptyAdd = useMemo(() => {
     return isEmpty(addStart) && isEmpty(addCenter) && isEmpty(addEnd);
@@ -14,31 +14,33 @@ const StatusBar = () => {
 
   return (
     <>
-      {!isEmpty(replaceContainer)
-        ? replaceContainer
-        : !isEmptyAdd && (
-            <div
-              className={
-                'flex h-7 w-full flex-row justify-between overflow-hidden border-t border-foreground/10' +
-                ' items-center bg-foreground-100/50 px-3 text-small dark:bg-LynxRaisinBlack/50'
-              }>
-              <div className={[classNames, 'justify-start'].join(' ')}>
-                {addStart.map((Start, index) => (
-                  <Start key={index} />
-                ))}
-              </div>
-              <div className={[classNames, 'justify-center'].join(' ')}>
-                {addCenter.map((Center, index) => (
-                  <Center key={index} />
-                ))}
-              </div>
-              <div className={[classNames, 'justify-end'].join(' ')}>
-                {addEnd.map((End, index) => (
-                  <End key={index} />
-                ))}
-              </div>
+      {!isNil(ReplaceContainer) ? (
+        <ReplaceContainer />
+      ) : (
+        !isEmptyAdd && (
+          <div
+            className={
+              'flex h-7 w-full flex-row justify-between overflow-hidden border-t border-foreground/10' +
+              ' items-center bg-foreground-100/50 px-3 text-small dark:bg-LynxRaisinBlack/50'
+            }>
+            <div className={[classNames, 'justify-start'].join(' ')}>
+              {addStart.map((Start, index) => (
+                <Start key={index} />
+              ))}
             </div>
-          )}
+            <div className={[classNames, 'justify-center'].join(' ')}>
+              {addCenter.map((Center, index) => (
+                <Center key={index} />
+              ))}
+            </div>
+            <div className={[classNames, 'justify-end'].join(' ')}>
+              {addEnd.map((End, index) => (
+                <End key={index} />
+              ))}
+            </div>
+          </div>
+        )
+      )}
     </>
   );
 };
