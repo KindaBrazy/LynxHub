da87de60872c5324652b51706edc4bd2980b3c69
kindabrazy <kindofbrazy@gmail.com>
2024-12-31 15:40:30 +0330

    chore(extensions_renderer): update example of adding card menu to using card data with context prop

--- src/renderer/extension/Components/Cards_AddMenu.tsx
+++ src/renderer/extension/Components/Cards_AddMenu.tsx
@@ -1,8 +1,14 @@
 import {DropdownItem, DropdownSection} from '@nextui-org/react';
+import {useEffect} from 'react';
 
+import {CardsDataManager} from '../../src/App/Components/Cards/CardsDataManager';
 import {Bug_Icon, Trash_Icon} from '../../src/assets/icons/SvgIcons/SvgIcons3';
 
-export default function CardsAddMenu() {
+export default function CardsAddMenu({context}: {context: CardsDataManager}) {
+  useEffect(() => {
+    console.log(context.id);
+  }, [context]);
+
   return (
     <DropdownSection key="bugs" title="Bugs" showDivider>
       <DropdownItem key="addBug" title="Add Bug" className="cursor-default" startContent={<Bug_Icon />}></DropdownItem>
