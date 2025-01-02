27b8dd33a3704ad41914ad5ef72c2a2776faa7ff
kindabrazy <kindofbrazy@gmail.com>
2025-01-02 17:20:45 +0330

    fix(modules): correct loading of modules

--- src/renderer/src/Layout.tsx
+++ src/renderer/src/Layout.tsx
@@ -1,15 +1,12 @@
 import {Provider as ReduxProvider} from 'react-redux';
 
 import App from './App/App';
-import ModulesProvider from './App/Modules/ModulesContext';
 import {createStore} from './App/Redux/Store';
 
 export default function Layout() {
   return (
     <ReduxProvider store={createStore()}>
-      <ModulesProvider>
-        <App />
-      </ModulesProvider>
+      <App />
     </ReduxProvider>
   );
 }
