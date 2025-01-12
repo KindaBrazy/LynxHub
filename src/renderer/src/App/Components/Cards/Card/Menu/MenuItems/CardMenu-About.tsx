2a128d4864043efd29e0971798c34376ab89e5b0
kindabrazy <kindofbrazy@gmail.com>
2025-01-12 19:21:55 +0330

    feat(card): open project folder via Ctrl+click on "Open Folder" in card menu

--- src/renderer/src/App/Components/Cards/Card/Menu/MenuItems/CardMenu-About.tsx
+++ src/renderer/src/App/Components/Cards/Card/Menu/MenuItems/CardMenu-About.tsx
@@ -1,31 +1,70 @@
 import {DropdownItem} from '@nextui-org/react';
-import {useCallback} from 'react';
+import {useCallback, useMemo, useState} from 'react';
+import {useHotkeys} from 'react-hotkeys-hook';
 import {useDispatch} from 'react-redux';
 
 import {Document_Icon} from '../../../../../../assets/icons/SvgIcons/SvgIcons1';
 import {Info_Icon} from '../../../../../../assets/icons/SvgIcons/SvgIcons2';
+import {OpenFolder_Icon} from '../../../../../../assets/icons/SvgIcons/SvgIcons4';
 import {modalActions} from '../../../../../Redux/AI/ModalsReducer';
 import {AppDispatch} from '../../../../../Redux/Store';
+import rendererIpc from '../../../../../RendererIpc';
 import {useDevInfo} from '../../../../../Utils/LocalStorage';
+import {useInstalledCard} from '../../../../../Utils/UtilHooks';
 import {useCardData} from '../../../CardsDataManager';
 
 export const MenuInfo = () => {
   const {id, extensionsDir, repoUrl, setMenuIsOpen, title} = useCardData();
+  const webUI = useInstalledCard(id);
   const {name} = useDevInfo(repoUrl);
+
   const dispatch = useDispatch<AppDispatch>();
 
-  const onPress = useCallback(() => {
-    dispatch(modalActions.openCardInfo({cardId: id, devName: name, extensionsDir, title, url: repoUrl}));
-    setMenuIsOpen(false);
-  }, [dispatch, setMenuIsOpen, id, name, extensionsDir, title, repoUrl]);
+  const [ctrlPressed, setCtrlPressed] = useState<boolean>(false);
+  const showOpenFolder = useMemo(() => {
+    return !!webUI?.dir && ctrlPressed;
+  }, [webUI, ctrlPressed]);
+
+  useHotkeys(
+    'ctrl',
+    () => {
+      setCtrlPressed(true);
+    },
+    {
+      keydown: true,
+      enableOnFormTags: true,
+      enableOnContentEditable: true,
+    },
+  );
+  useHotkeys(
+    'ctrl',
+    () => {
+      setCtrlPressed(false);
+    },
+    {
+      keyup: true,
+      enableOnFormTags: true,
+      enableOnContentEditable: true,
+    },
+  );
+
+  const onPress = () => {
+    if (showOpenFolder) {
+      rendererIpc.file.openPath(webUI!.dir!);
+      setCtrlPressed(false);
+    } else {
+      dispatch(modalActions.openCardInfo({cardId: id, devName: name, extensionsDir, title, url: repoUrl}));
+      setMenuIsOpen(false);
+    }
+  };
 
   return (
     <DropdownItem
       key="information"
       onPress={onPress}
-      title="Information"
       className="cursor-default"
-      startContent={<Info_Icon />}
+      title={showOpenFolder ? 'Open Folder' : 'Information'}
+      startContent={showOpenFolder ? <OpenFolder_Icon /> : <Info_Icon />}
     />
   );
 };
