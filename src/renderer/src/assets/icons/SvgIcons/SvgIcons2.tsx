d0d51984e0f10a3976915bd66f959f83e3fb036b
kindabrazy <kindofbrazy@gmail.com>
2025-01-13 13:58:31 +0330

    refactor(menu): rename ReadMe to HomePage

--- src/renderer/src/assets/icons/SvgIcons/SvgIcons2.tsx
+++ src/renderer/src/assets/icons/SvgIcons/SvgIcons2.tsx
@@ -61,6 +61,27 @@ export function Home_Icon(props: SvgProps): ReactElement {
   );
 }
 
+export function HomeSmile_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <rect width="24" height="24" fill="none" />
+      <path
+        d={
+          'M13.106 22h-2.212c-3.447 0-5.17 0-6.345-1.012s-1.419-2.705-1.906-6.093l-.279-1.937c-.38' +
+          '-2.637-.57-3.956-.029-5.083s1.691-1.813 3.992-3.183l1.385-.825C9.8 2.622 10.846 2 12 2' +
+          's2.199.622 4.288 1.867l1.385.825c2.3 1.37 3.451 2.056 3.992 3.183s.35 2.446-.03 5.083l-' +
+          '.278 1.937c-.487 3.388-.731 5.081-1.906 6.093S16.553 22 13.106 22m-4.708-6.447a.75.75 0 ' +
+          '0 1 1.049-.156c.728.54 1.607.853 2.553.853s1.825-.313 2.553-.853a.75.75 0 1 1 .894 1.205A' +
+          '5.77 5.77 0 0 1 12 17.75a5.77 5.77 0 0 1-3.447-1.148a.75.75 0 0 1-.155-1.049'
+        }
+        fillRule="evenodd"
+        clipRule="evenodd"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
 export function ImageGeneration_Icon(props: SvgProps): ReactElement {
   return (
     <svg {...props} width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
