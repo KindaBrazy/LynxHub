f4d8f7ae2b5578cd0d883075cc8cd68663a049ba
kindabrazy <kindofbrazy@gmail.com>
2025-01-18 15:23:31 +0330

    feat(card): hide uninstall menu item if card has no installed directory

--- src/renderer/src/App/Components/Cards/Card/Menu/MenuItems/CardMenu-Danger.tsx
+++ src/renderer/src/App/Components/Cards/Card/Menu/MenuItems/CardMenu-Danger.tsx
@@ -5,16 +5,21 @@ import {useDispatch} from 'react-redux';
 import {Trash_Icon} from '../../../../../../assets/icons/SvgIcons/SvgIcons3';
 import {modalActions} from '../../../../../Redux/AI/ModalsReducer';
 import {AppDispatch} from '../../../../../Redux/Store';
+import {useInstalledCard} from '../../../../../Utils/UtilHooks';
 import {useCardData} from '../../../CardsDataManager';
 
 export function MenuUninstall() {
   const {id, setMenuIsOpen} = useCardData();
+  const card = useInstalledCard(id);
+
   const dispatch = useDispatch<AppDispatch>();
 
   const onPress = useCallback(() => {
     dispatch(modalActions.openUninstallCard(id));
     setMenuIsOpen(false);
   }, [dispatch, setMenuIsOpen, id]);
+
+  if (!card?.dir) return <DropdownItem className="hidden" key="uninstall-hidden" textValue="uninstall_hidden" />;
 
   return (
     <DropdownItem
