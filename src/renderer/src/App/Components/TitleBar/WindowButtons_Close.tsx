6885e87efec2f54ef41231b815bf1e6dde72936d
kindabrazy <kindofbrazy@gmail.com>
2025-02-02 15:11:38 +0330

    feat(ui): reposition "Close" pop confirm under button

---
+++ src/renderer/src/App/Components/TitleBar/WindowButtons_Close.tsx
@@ -0,0 +1,105 @@
+import {Button, Checkbox, Popover, PopoverContent, PopoverTrigger} from '@heroui/react';
+import {Typography} from 'antd';
+import {motion} from 'framer-motion';
+import {useCallback, useState} from 'react';
+import {isHotkeyPressed} from 'react-hotkeys-hook';
+import {useDispatch} from 'react-redux';
+
+import {Power_Icon} from '../../../assets/icons/SvgIcons/SvgIcons2';
+import {useAppState} from '../../Redux/App/AppReducer';
+import {settingsActions, useSettingsState} from '../../Redux/App/SettingsReducer';
+import {AppDispatch} from '../../Redux/Store';
+import rendererIpc from '../../RendererIpc';
+
+type Props = {
+  buttonProps: any;
+  commonStyles: string;
+};
+
+export default function WindowButtons_Close({buttonProps, commonStyles}: Props) {
+  const currentPage = useAppState('currentPage');
+  const showCloseConfirm = useSettingsState('closeConfirm');
+  const dispatch = useDispatch<AppDispatch>();
+
+  const [isConfirmOpen, setIsConfirmOpen] = useState<boolean>(false);
+
+  const restart = useCallback(() => {
+    rendererIpc.pty.process('stop', '');
+    setTimeout(() => rendererIpc.win.changeWinState('restart'), 200);
+  }, []);
+
+  const onClick = () => {
+    rendererIpc.storage.update('app', {lastPage: currentPage});
+    if (isHotkeyPressed('control') || !showCloseConfirm) {
+      close();
+    } else if (!isConfirmOpen) {
+      setIsConfirmOpen(true);
+    }
+  };
+
+  const onShowConfirm = useCallback(
+    (enabled: boolean) => {
+      rendererIpc.storage.update('app', {closeConfirm: !enabled});
+      dispatch(settingsActions.setSettingsState({key: 'closeConfirm', value: !enabled}));
+    },
+    [dispatch],
+  );
+
+  const close = useCallback(() => {
+    rendererIpc.pty.process('stop', '');
+    setTimeout(() => rendererIpc.win.changeWinState('close'), 200);
+  }, []);
+
+  return (
+    <Popover
+      onClick={e => {
+        // @ts-expect-error
+        if (e.target.dataset.slot === 'backdrop') {
+          setIsConfirmOpen(false);
+        }
+      }}
+      backdrop="opaque"
+      placement="bottom-end"
+      isOpen={isConfirmOpen}
+      onOpenChange={setIsConfirmOpen}
+      classNames={{base: 'before:dark:bg-LynxRaisinBlack', backdrop: '!top-10'}}
+      showArrow>
+      <PopoverTrigger>
+        <motion.button
+          {...buttonProps}
+          type="button"
+          onClick={onClick}
+          className={`${commonStyles} pl-3 pr-[1rem] hover:bg-red-600`}>
+          <Power_Icon className="size-[0.8rem]" />
+        </motion.button>
+      </PopoverTrigger>
+      <PopoverContent className="py-4 px-8 dark:bg-LynxRaisinBlack">
+        <span className="self-start text-medium font-semibold">Confirm Exit</span>
+        <div className="mt-2 flex flex-col space-y-1">
+          <Typography.Text>Are you sure you want to exit the application?</Typography.Text>
+          <Checkbox size="sm" onValueChange={onShowConfirm}>
+            Always exit without confirmation
+          </Checkbox>
+        </div>
+        <div className="mt-2 flex w-full flex-row justify-between">
+          <Button
+            onPress={() => {
+              setIsConfirmOpen(false);
+            }}
+            size="sm"
+            color="success">
+            Stay
+          </Button>
+          <div className="space-x-2">
+            <Button size="sm" color="warning" onPress={restart}>
+              Restart
+            </Button>
+            <Button size="sm" color="danger" onPress={close}>
+              Exit
+            </Button>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
