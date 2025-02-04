c08942bfaf6f32fb61dc3c83452c4c53a95d242e
kindabrazy <kindofbrazy@gmail.com>
2025-02-04 13:46:09 +0330

    style(markdown): make readme modal close button smaller

--- src/renderer/src/App/Components/Modals/CardReadme_Modal.tsx
+++ src/renderer/src/App/Components/Modals/CardReadme_Modal.tsx
@@ -57,6 +57,7 @@ const CardReadmeModal = () => {
             onPress={() => {
               onOpenChange(false);
             }}
+            size="sm"
             variant="flat"
             color="warning"
             fullWidth>
