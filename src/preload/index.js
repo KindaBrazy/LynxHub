fbcae781b1989a6ee20d70a20c1ae607a0005604
kindabrazy <kindofbrazy@gmail.com>
2025-02-13 13:20:13 +0330

    feat(preload,renderer): add isPortable value to process

--- src/preload/index.js
+++ src/preload/index.js
@@ -2,15 +2,18 @@ import os from 'node:os';
 
 import {electronAPI} from '@electron-toolkit/preload';
 import {contextBridge} from 'electron';
+import {isPortable} from 'src/preload/utils.js';
 
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI);
     contextBridge.exposeInMainWorld('osPlatform', os.platform());
+    contextBridge.exposeInMainWorld('isPortable', isPortable());
   } catch (error) {
     console.error(error);
   }
 } else {
   window.electron = electronAPI;
   window.osPlatform = os.platform();
+  window.isPortable = isPortable();
 }
