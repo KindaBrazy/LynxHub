2f8b55d4c6e94c497b0d7c14d3ab0214416fb5fd
kindabrazy <kindofbrazy@gmail.com>
2025-02-14 13:25:30 +0330

    feat(icons): add new Clock SVG icon

--- src/renderer/src/assets/icons/SvgIcons/SvgIcons5.tsx
+++ src/renderer/src/assets/icons/SvgIcons/SvgIcons5.tsx
@@ -181,3 +181,21 @@ export function CloseSimple_Icon(props: SvgProps): ReactElement {
     </svg>
   );
 }
+
+export function Clock_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10"
+        opacity="0.5"
+      />
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75"
+        clip-rule="evenodd"
+      />
+    </svg>
+  );
+}
