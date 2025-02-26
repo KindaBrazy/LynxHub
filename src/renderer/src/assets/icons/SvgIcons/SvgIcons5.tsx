2b7524533870b7e7995d5199df1b6f5a0e097b04
kindabrazy <kindofbrazy@gmail.com>
2025-02-26 20:59:40 +0330

    feat(icons): add new ArrowDuo SVG icon

--- src/renderer/src/assets/icons/SvgIcons/SvgIcons5.tsx
+++ src/renderer/src/assets/icons/SvgIcons/SvgIcons5.tsx
@@ -268,3 +268,18 @@ export function ShieldCross_Icon(props: SvgProps): ReactElement {
     </svg>
   );
 }
+
+export function ArrowDuo_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        opacity="0.5"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        fill="currentColor"
+        d="M20.75 12a.75.75 0 0 0-.75-.75h-9.25v1.5H20a.75.75 0 0 0 .75-.75"
+      />
+      <path fill="currentColor" d="M10.75 18a.75.75 0 0 1-1.28.53l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.28.53z" />
+    </svg>
+  );
+}
