34f3d8a1cfb325a18f91f61b41ef100f6b7f311d
kindabrazy <kindofbrazy@gmail.com>
2025-02-27 19:30:08 +0330

    feat(tabs): add animations for tabs moving and closing

--- src/renderer/src/App/Components/Tabs/TabContainer.tsx
+++ src/renderer/src/App/Components/Tabs/TabContainer.tsx
@@ -1,6 +1,7 @@
 import {Divider} from 'antd';
+import {AnimatePresence, motion} from 'framer-motion';
 import {generate} from 'random-words';
-import {Fragment, useState} from 'react';
+import {useState} from 'react';
 
 import {Web_Icon} from '../../../assets/icons/SvgIcons/SvgIcons3';
 import NewTab from './NewTab';
@@ -19,12 +20,8 @@ export default function TabContainer() {
 
   const [isSelected, setIsSelected] = useState<string>('');
 
-  const removeTab = (index: number) => {
-    setItems(prevState => {
-      prevState.splice(index, 1);
-
-      return [...prevState];
-    });
+  const removeTab = (title: string) => {
+    setItems(prevState => prevState.filter(item => item !== title));
   };
 
   return (
@@ -33,18 +30,30 @@ export default function TabContainer() {
         'h-full items-center pt-1 notDraggable justify-between overflow-hidden flex flex-row pl-1 gap-x-1 relative'
       }>
       <div className="items-center h-full flex flex-row overflow-x-scroll scrollbar-hide">
-        {items.map((title, index) => (
-          <Fragment key={index}>
-            <TabItem
-              title={title}
-              setIsSelected={setIsSelected}
-              isSelected={isSelected === title}
-              removeTab={() => removeTab(index)}
-              icon={<Web_Icon className="size-full" />}
-            />
-            {index < items.length - 1 && <Divider type="vertical" className="mx-1" />}
-          </Fragment>
-        ))}
+        <AnimatePresence>
+          {items.map((title, index) => (
+            <motion.div
+              transition={{
+                layout: {duration: 0.5, type: 'spring'},
+              }}
+              key={title}
+              layout={'position'}
+              className="flex items-center"
+              initial={{scale: 0.8, y: 10, x: 20, opacity: 0}}
+              exit={{scale: 0.5, y: 10, x: 20, transition: {duration: 0.07, ease: 'backIn'}}}
+              animate={{scale: 1, y: 0, x: 0, opacity: 1, transition: {duration: 0.25, ease: 'backOut'}}}>
+              <TabItem
+                key={title}
+                title={title}
+                setIsSelected={setIsSelected}
+                isSelected={isSelected === title}
+                removeTab={() => removeTab(title)}
+                icon={<Web_Icon className="size-full" />}
+              />
+              {index < items.length - 1 && <Divider type="vertical" className="mx-1" />}
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </div>
 
       <NewTab addTab={addTab} />
--- src/renderer/src/App/Components/Tabs/TabItem.tsx
+++ src/renderer/src/App/Components/Tabs/TabItem.tsx
@@ -1,5 +1,4 @@
 import {Button} from '@heroui/react';
-import {motion} from 'framer-motion';
 import {ReactNode, useEffect, useRef} from 'react';
 
 import {Terminal_Icon} from '../../../assets/icons/SvgIcons/SvgIcons3';
@@ -30,30 +29,25 @@ export default function TabItem({title, icon, isSelected, isTerminal, setIsSelec
   const onPress = () => setIsSelected(title || '');
 
   return (
-    <motion.div
-      animate={{scale: 1, x: 0, opacity: 1}}
-      initial={{scale: 0.8, x: 20, opacity: 0}}
-      transition={{duration: 0.25, ease: 'backOut'}}>
-      <Button
-        className={
-          'h-full group rounded-t-lg max-w-60 min-w-[3.3rem] items-center pr-0 ' +
-          'overflow-hidden text-small pl-2 flex data-[hover=true]:bg-foreground-100 flex-row relative cursor-default ' +
-          `${isSelected && 'bg-white dark:bg-[#303033]'}`
-        }
-        ref={btnRef}
-        radius="none"
-        variant="light"
-        onPress={onPress}>
-        <div className="flex gap-x-2 flex-row items-center min-w-0 flex-1">
-          {isTerminal && <Terminal_Icon className="opacity-70 shrink-0" />}
-          <div className="shrink-0 size-4 content-center">{icon}</div>
-          <span className="truncate">{title}</span>
-        </div>
+    <Button
+      className={
+        'h-full group rounded-t-lg max-w-60 min-w-[3.3rem] items-center pr-0 ' +
+        'overflow-hidden text-small pl-2 flex data-[hover=true]:bg-foreground-100 flex-row relative cursor-default ' +
+        `${isSelected && 'bg-white dark:bg-[#303033]'}`
+      }
+      ref={btnRef}
+      radius="none"
+      variant="light"
+      onPress={onPress}>
+      <div className="flex gap-x-2 flex-row items-center min-w-0 flex-1">
+        {isTerminal && <Terminal_Icon className="opacity-70 shrink-0" />}
+        <div className="shrink-0 size-4 content-center">{icon}</div>
+        <span className="truncate">{title}</span>
+      </div>
 
-        <Button as="span" size="sm" variant="light" onPress={removeTab} className="scale-75 cursor-default" isIconOnly>
-          <CloseSimple_Icon className="size-4" />
-        </Button>
+      <Button as="span" size="sm" variant="light" onPress={removeTab} className="scale-75 cursor-default" isIconOnly>
+        <CloseSimple_Icon className="size-4" />
       </Button>
-    </motion.div>
+    </Button>
   );
 }
