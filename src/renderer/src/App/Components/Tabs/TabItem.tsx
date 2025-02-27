03e915ebdc5b7c56522092b9e3b6e9be505dbb14
kindabrazy <kindofbrazy@gmail.com>
2025-02-27 11:28:23 +0330

    feat(tabs): implement basic tab removal, middle-click close, and new tab animation

--- src/renderer/src/App/Components/Tabs/TabItem.tsx
+++ src/renderer/src/App/Components/Tabs/TabItem.tsx
@@ -1,5 +1,6 @@
 import {Button} from '@heroui/react';
-import {ReactNode} from 'react';
+import {motion} from 'framer-motion';
+import {ReactNode, useEffect, useRef} from 'react';
 
 import {Terminal_Icon} from '../../../assets/icons/SvgIcons/SvgIcons3';
 import {CloseSimple_Icon} from '../../../assets/icons/SvgIcons/SvgIcons5';
@@ -10,28 +11,49 @@ type Props = {
   icon?: ReactNode;
   isTerminal?: boolean;
   setIsSelected: (selected: string) => void;
+  removeTab: () => void;
 };
 
-export default function TabItem({title, icon, isSelected, isTerminal, setIsSelected}: Props) {
-  return (
-    <Button
-      className={
-        'h-full group rounded-t-lg max-w-60 min-w-[3.3rem] items-center pr-0 ' +
-        'overflow-hidden text-small pl-2 flex data-[hover=true]:bg-foreground-100 flex-row relative cursor-default ' +
-        `${isSelected && 'bg-white dark:bg-[#303033]'}`
-      }
-      radius="none"
-      variant="light"
-      onPress={() => setIsSelected(title || '')}>
-      <div className="flex gap-x-2 flex-row items-center min-w-0 flex-1">
-        {isTerminal && <Terminal_Icon className="opacity-70 shrink-0" />}
-        <div className="shrink-0 size-4 content-center">{icon}</div>
-        <span className="truncate">{title}</span>
-      </div>
+export default function TabItem({title, icon, isSelected, isTerminal, setIsSelected, removeTab}: Props) {
+  const btnRef = useRef<HTMLButtonElement | null>(null);
+  useEffect(() => {
+    if (btnRef.current) {
+      btnRef.current.addEventListener('auxclick', (e: MouseEvent) => {
+        if (e.button === 1) {
+          e.preventDefault();
+          removeTab();
+        }
+      });
+    }
+  }, [btnRef]);
 
-      <Button as="span" size="sm" variant="light" className="scale-75 cursor-default" isIconOnly>
-        <CloseSimple_Icon className="size-4" />
+  const onPress = () => setIsSelected(title || '');
+
+  return (
+    <motion.div
+      animate={{scale: 1, x: 0, opacity: 1}}
+      initial={{scale: 0.8, x: 20, opacity: 0}}
+      transition={{duration: 0.25, ease: 'backOut'}}>
+      <Button
+        className={
+          'h-full group rounded-t-lg max-w-60 min-w-[3.3rem] items-center pr-0 ' +
+          'overflow-hidden text-small pl-2 flex data-[hover=true]:bg-foreground-100 flex-row relative cursor-default ' +
+          `${isSelected && 'bg-white dark:bg-[#303033]'}`
+        }
+        ref={btnRef}
+        radius="none"
+        variant="light"
+        onPress={onPress}>
+        <div className="flex gap-x-2 flex-row items-center min-w-0 flex-1">
+          {isTerminal && <Terminal_Icon className="opacity-70 shrink-0" />}
+          <div className="shrink-0 size-4 content-center">{icon}</div>
+          <span className="truncate">{title}</span>
+        </div>
+
+        <Button as="span" size="sm" variant="light" onPress={removeTab} className="scale-75 cursor-default" isIconOnly>
+          <CloseSimple_Icon className="size-4" />
+        </Button>
       </Button>
-    </Button>
+    </motion.div>
   );
 }
