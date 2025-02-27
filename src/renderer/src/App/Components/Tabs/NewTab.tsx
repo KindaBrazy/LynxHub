0b6b33bb4171b35a2c0966012d42eab8c4d432f3
kindabrazy <kindofbrazy@gmail.com>
2025-02-27 19:32:27 +0330

    feat(tabs): animate new tab button movement

--- src/renderer/src/App/Components/Tabs/NewTab.tsx
+++ src/renderer/src/App/Components/Tabs/NewTab.tsx
@@ -1,12 +1,15 @@
 import {Button} from '@heroui/react';
+import {motion} from 'framer-motion';
 
 import {Add_Icon} from '../../../assets/icons/SvgIcons/SvgIcons1';
 
 type Props = {addTab: () => void};
 export default function NewTab({addTab}: Props) {
   return (
-    <Button size="sm" variant="light" onPress={addTab} className="cursor-default mb-1" isIconOnly>
-      <Add_Icon className="size-[0.9rem]" />
-    </Button>
+    <motion.div layoutId="new_tab" transition={{duration: 0.37, type: 'spring'}} layout>
+      <Button size="sm" variant="light" onPress={addTab} className="cursor-default mb-1" isIconOnly>
+        <Add_Icon className="size-[0.9rem]" />
+      </Button>
+    </motion.div>
   );
 }
