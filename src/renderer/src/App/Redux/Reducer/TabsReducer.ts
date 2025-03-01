1c83e39fe5721a5d8be9d7074beae65ef208e26c
kindabrazy <kindofbrazy@gmail.com>
2025-03-01 20:37:07 +0330

    fix(tabs_reducer): handle duplicate IDs in addTab reducer

--- src/renderer/src/App/Redux/Reducer/TabsReducer.ts
+++ src/renderer/src/App/Redux/Reducer/TabsReducer.ts
@@ -34,8 +34,22 @@ const tabsSlice = createSlice({
     setAppState: <K extends keyof TabState>(state: TabState, action: PayloadAction<{key: K; value: TabState[K]}>) => {
       state[action.payload.key] = action.payload.value;
     },
-    addTab: (state: TabState, action: PayloadAction<Tab>) => {
-      state.tabs.push(action.payload);
+    addTab: (state: TabState, action: PayloadAction<TabInfo>) => {
+      let newID = action.payload.id;
+      let idNumber = 1;
+
+      const checkDuplicateId = () => {
+        const existTab = state.tabs.find(tab => tab.id === newID);
+        if (existTab) {
+          newID = `action.payload.id_${idNumber}`;
+          idNumber++;
+          checkDuplicateId();
+        }
+      };
+
+      checkDuplicateId();
+
+      state.tabs.push({...action.payload, id: newID});
     },
     removeTab: (state: TabState, action: PayloadAction<string>) => {
       state.tabs = state.tabs.filter(tab => tab.id !== action.payload);
