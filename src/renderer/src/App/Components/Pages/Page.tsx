1767756cb2c4cff5e00bc80ad32e01556619e33d
kindabrazy <kindofbrazy@gmail.com>
2025-03-01 20:25:53 +0330

    style(ui): reduce blur intensity on page transitions

--- src/renderer/src/App/Components/Pages/Page.tsx
+++ src/renderer/src/App/Components/Pages/Page.tsx
@@ -14,7 +14,7 @@ export default function Page({children, className}: Props) {
       className={['size-full py-2.5', className].join(' ')}
       transition={{duration: 0.3, ease: [0.25, 0.1, 0.25, 1]}}
       animate={{filter: 'blur(0px)', opacity: 1, transform: 'translateY(0%)'}}
-      initial={{filter: 'blur(10px)', opacity: 0, transform: 'translateY(-2%)'}}>
+      initial={{filter: 'blur(3px)', opacity: 0, transform: 'translateY(-2%)'}}>
       {children}
     </motion.div>
   );
