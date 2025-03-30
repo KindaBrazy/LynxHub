8a8a16e214b9ee564e137155f1aa1233c083913a
kindabrazy <kindofbrazy@gmail.com>
2025-03-30 16:44:55 +0330

    feat(installer): show error on selecting LynxHub installation directory to avoid data loss #29

--- src/renderer/src/App/Components/Reusable/OpenDialog.tsx
+++ src/renderer/src/App/Components/Reusable/OpenDialog.tsx
@@ -1,6 +1,6 @@
 import {Button, ButtonGroup, Input} from '@heroui/react';
 import {OpenDialogOptions} from 'electron';
-import {useCallback, useRef} from 'react';
+import {useCallback, useRef, useState} from 'react';
 
 import {Folder_Icon} from '../../../assets/icons/SvgIcons/SvgIcons1';
 import rendererIpc from '../../RendererIpc';
@@ -15,6 +15,7 @@ type Props = {
 /** Input field with a button to open a file/directory dialog. */
 export default function OpenDialog({dialogType, directory, extraFolder = '', setDirectory}: Props) {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [isInvalid, setIsInvalid] = useState<boolean>(false);
 
   const changeDirectory = useCallback(
     (value: string) => {
@@ -33,12 +34,15 @@ export default function OpenDialog({dialogType, directory, extraFolder = '', set
         const resultDir = extraFolder ? `${result}${isWin ? '\\' : '/'}${extraFolder}` : result;
         setDirectory(resultDir);
         inputRef.current.value = resultDir;
+        rendererIpc.appData.isAppDir(resultDir).then(isAppDir => {
+          setIsInvalid(isAppDir);
+        });
       }
     });
   }, [dialogType, extraFolder, setDirectory]);
 
   return (
-    <ButtonGroup fullWidth>
+    <ButtonGroup className="items-start" fullWidth>
       <Input
         classNames={{
           inputWrapper: 'rounded-r-none !transition !duration-150',
@@ -47,11 +51,14 @@ export default function OpenDialog({dialogType, directory, extraFolder = '', set
         color="primary"
         value={directory}
         variant="bordered"
+        isInvalid={isInvalid}
         defaultValue={directory}
         aria-label="Directory path"
         onValueChange={changeDirectory}
+        errorMessage="Selecting the app folder can be dangerous and may lead to data loss."
         multiple
       />
+
       <Button variant="solid" onPress={chooseDirectory} aria-label="Choose directory" isIconOnly>
         {<Folder_Icon className="size-[20px]" />}
       </Button>
