a5f59905f7ef067e0d14c8cf7b4c18314fecc3df
kindabrazy <kindofbrazy@gmail.com>
2025-04-11 19:41:02 +0330

    feat(browser): Implement empty browser functionality

--- src/renderer/src/App/Components/Browser_Terminal/Browser/Browser_TopBar.tsx
+++ src/renderer/src/App/Components/Browser_Terminal/Browser/Browser_TopBar.tsx
@@ -5,13 +5,18 @@ import AddressInput from './AddressInput';
 import Browser_ActionButtons from './Browser_ActionButtons';
 import Browser_Zoom from './Browser_Zoom';
 
-type Props = {webview: WebviewTag | null; isDomReady: boolean; runningCard: RunningCard};
+type Props = {
+  webview: WebviewTag | null;
+  isDomReady: boolean;
+  runningCard: RunningCard;
+  setCustomAddress?: (address: string) => void;
+};
 
-export default function Browser_TopBar({webview, isDomReady, runningCard}: Props) {
+export default function Browser_TopBar({webview, isDomReady, runningCard, setCustomAddress}: Props) {
   return (
     <>
       <Browser_ActionButtons webview={webview} isDomReady={isDomReady} webuiAddress={runningCard.webUIAddress} />
-      <AddressInput runningCard={runningCard} />
+      <AddressInput runningCard={runningCard} setCustomAddress={setCustomAddress} />
       <Browser_Zoom id={runningCard.id} />
     </>
   );
