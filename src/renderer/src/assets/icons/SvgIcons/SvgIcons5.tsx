b223d8bfd8dc0fdac1b8899861c5c4590312b3fd
kindabrazy <kindofbrazy@gmail.com>
2025-04-15 12:14:30 +0330

    feat(icons): Add new SVG icon: PenDuo_Icon

--- src/renderer/src/assets/icons/SvgIcons/SvgIcons5.tsx
+++ src/renderer/src/assets/icons/SvgIcons/SvgIcons5.tsx
@@ -346,3 +346,27 @@ export function EraserDuo_Icon(props: SvgProps): ReactElement {
     </svg>
   );
 }
+
+export function PenDuo_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d={
+          'M20.849 8.713a3.932 3.932 0 0 0-5.562-5.561l-.887.887l.038.111a8.75 8.75' +
+          ' 0 0 0 2.093 3.32a8.75 8.75 0 0 0 3.43 2.13z'
+        }
+        opacity="0.5"
+        fill="currentColor"
+      />
+      <path
+        d={
+          'm14.439 4l-.039.038l.038.112a8.75 8.75 0 0 0 2.093 3.32a8.75 8.75 0 0 0 3.43' +
+          ' 2.13l-8.56 8.56c-.578.577-.867.866-1.185 1.114a6.6 6.6 0 0 1-1.211.748c-.364' +
+          '.174-.751.303-1.526.561l-4.083 1.361a1.06 1.06 0 0 1-1.342-1.341l1.362-4.084c.2' +
+          '58-.774.387-1.161.56-1.525q.309-.646.749-1.212c.248-.318.537-.606 1.114-1.183z'
+        }
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
