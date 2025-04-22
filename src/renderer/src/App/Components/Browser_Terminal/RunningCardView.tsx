e2b7c4205c00ddeda055efe78926a4743b25495e
kindabrazy <kindofbrazy@gmail.com>
2025-04-22 16:46:50 +0330

    feat(tabs): update tab title on view change and terminate

--- src/renderer/src/App/Components/Browser_Terminal/RunningCardView.tsx
+++ src/renderer/src/App/Components/Browser_Terminal/RunningCardView.tsx
@@ -4,6 +4,7 @@ import {useCallback, useEffect, useMemo, useState} from 'react';
 import {useDispatch} from 'react-redux';
 
 import {extensionsData} from '../../Extensions/ExtensionLoader';
+import {useAllCards} from '../../Modules/ModuleLoader';
 import {cardsActions} from '../../Redux/Reducer/CardsReducer';
 import {tabsActions, useTabsState} from '../../Redux/Reducer/TabsReducer';
 import {AppDispatch} from '../../Redux/Store';
@@ -20,11 +21,26 @@ const RunningCardView = ({runningCard}: Props) => {
   const ExtTerminal = useMemo(() => extensionsData.runningAI.terminal, []);
   const ExtBrowser = useMemo(() => extensionsData.runningAI.browser, []);
 
+  const allCards = useAllCards();
+
   const activeTab = useTabsState('activeTab');
+  const tabs = useTabsState('tabs');
   const dispatch = useDispatch<AppDispatch>();
 
   const [webViewRef, setWebViewRef] = useState<WebviewTag | null>(null);
   const [isDomReady, setIsDomReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    const isBrowserView = runningCard.currentView === 'browser';
+
+    const terminalTitle = allCards.find(card => card.id === runningCard.id)?.title;
+    const browserTitle = webViewRef && isDomReady ? webViewRef.getTitle() : undefined;
+
+    const title = isBrowserView ? browserTitle : terminalTitle;
+
+    const currentTitle = tabs.find(tab => tab.id === activeTab)?.title;
+    if (title && title !== currentTitle) dispatch(tabsActions.setActiveTabTitle(title));
+  }, [runningCard, webViewRef, tabs, activeTab, isDomReady]);
 
   useEffect(() => {
     if (webViewRef) {
@@ -45,12 +61,12 @@ const RunningCardView = ({runningCard}: Props) => {
       webViewRef.addEventListener('did-start-loading', () => onLoading(true));
       webViewRef.addEventListener('did-stop-loading', () => onLoading(false));
 
-      webViewRef.addEventListener('page-title-updated', () => {
-        dispatch(tabsActions.setActiveTabTitle(webViewRef.getTitle()));
-      });
+      const setTitle = () => dispatch(tabsActions.setActiveTabTitle(webViewRef.getTitle()));
+      webViewRef.addEventListener('page-title-updated', () => setTitle);
 
       return () => {
         webViewRef.removeEventListener('did-start-navigation', didNavigate);
+        webViewRef.removeEventListener('page-title-updated', setTitle);
         webViewRef.removeEventListener('did-start-loading', () => onLoading(true));
         webViewRef.removeEventListener('did-stop-loading', () => onLoading(false));
       };
