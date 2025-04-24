38ad6254ff7d84cd905403fca8ad3af09d6a0ab1
kindabrazy <kindofbrazy@gmail.com>
2025-04-24 16:59:00 +0330

    feat(utils_main): add "getUserAgent" function to determine user agent string based on OS

--- src/main/Utilities/Utils.ts
+++ src/main/Utilities/Utils.ts
@@ -176,3 +176,23 @@ export function RelaunchApp(saveLastSize: boolean = true) {
   app.relaunch({execPath: process.env.PORTABLE_EXECUTABLE_FILE || process.env.APPIMAGE});
   app.exit();
 }
+
+export function getUserAgent() {
+  const os = platform();
+
+  switch (os) {
+    case 'darwin':
+      return (
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36' +
+        ' (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'
+      );
+    case 'linux':
+      return 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36';
+    case 'win32':
+    default:
+      return (
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36' +
+        ' (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'
+      );
+  }
+}
