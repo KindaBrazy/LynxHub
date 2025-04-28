03531efdf3a7c978cf578af578c4c983a7b35b39
kindabrazy <kindofbrazy@gmail.com>
2025-04-28 11:07:09 +0330

    feat(copy-clipboard): add support for custom className prop for enhanced styling

--- src/renderer/src/App/Components/Reusable/CopyClipboard.tsx
+++ src/renderer/src/App/Components/Reusable/CopyClipboard.tsx
@@ -3,9 +3,9 @@ import {useCallback, useState} from 'react';
 
 import {CheckDuo_Icon, CopyDuo_Icon} from '../../../assets/icons/SvgIcons/SvgIcons5';
 
-type Props = {tooltipTitle?: string; showTooltip?: boolean; contentToCopy: string};
+type Props = {tooltipTitle?: string; showTooltip?: boolean; contentToCopy: string; className?: string};
 
-export default function CopyClipboard({showTooltip = true, tooltipTitle, contentToCopy}: Props) {
+export default function CopyClipboard({showTooltip = true, tooltipTitle, contentToCopy, className}: Props) {
   const [copied, setCopied] = useState<boolean>(false);
 
   const handleCopy = useCallback(() => {
@@ -23,7 +23,7 @@ export default function CopyClipboard({showTooltip = true, tooltipTitle, content
       isDisabled={!showTooltip}
       content={copied ? 'Copied!' : tooltipTitle || 'Copy to clipboard'}
       showArrow>
-      <Button size="sm" variant="light" onPress={handleCopy} className="cursor-default" isIconOnly>
+      <Button size="sm" variant="light" onPress={handleCopy} className={`cursor-default ${className}`} isIconOnly>
         {copied ? (
           <CheckDuo_Icon className="size-5 animate-appearance-in" />
         ) : (
