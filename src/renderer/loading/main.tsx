ce4a281eb0db444d8bbba8613bb3155c90abd361
kindabrazy <kindofbrazy@gmail.com>
2025-05-02 16:35:12 +0330

    feat(loading): replace AppLoading with randomized loading animations selector

--- src/renderer/loading/main.tsx
+++ src/renderer/loading/main.tsx
@@ -2,6 +2,29 @@ import './index.css';
 
 import {createRoot} from 'react-dom/client';
 
-import App from './AppLoading';
+import LiquidChromeLoading from './Loadings/LiquidChromeLoading';
+import OrbLoading from './Loadings/OrbLoading';
+import ThreadsLoading from './Loadings/ThreadsLoading';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+const loadings = [
+  {component: <OrbLoading />, weight: 100},
+  {component: <ThreadsLoading />, weight: 100},
+  {component: <LiquidChromeLoading />, weight: 20},
+];
+
+const totalWeight = loadings.reduce((sum, loading) => sum + loading.weight, 0);
+
+const randomNumber = Math.random() * totalWeight;
+
+let selectedLoading = loadings[0].component;
+let cumulativeWeight = 0;
+
+for (const loading of loadings) {
+  cumulativeWeight += loading.weight;
+  if (randomNumber < cumulativeWeight) {
+    selectedLoading = loading.component;
+    break;
+  }
+}
+
+createRoot(document.getElementById('root') as HTMLElement).render(selectedLoading);
