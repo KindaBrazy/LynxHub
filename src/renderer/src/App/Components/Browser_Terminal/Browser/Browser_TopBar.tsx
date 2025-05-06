165b21c1765446cac8a1b55d82e7fe3dbe09c7f8
kindabrazy <kindofbrazy@gmail.com>
2025-05-06 19:36:50 +0330

    feat(browser): add find-in-page functionality to browser component

--- src/renderer/src/App/Components/Browser_Terminal/Browser/Browser_TopBar.tsx
+++ src/renderer/src/App/Components/Browser_Terminal/Browser/Browser_TopBar.tsx
@@ -4,6 +4,7 @@ import {RunningCard} from '../../../Utils/Types';
 import AddressInput from './AddressInput';
 import Browser_ActionButtons from './Browser_ActionButtons';
 import Browser_Zoom from './Browser_Zoom';
+import BrowserSearch from './BrowserSearch';
 
 type Props = {
   webview: WebviewTag | null;
@@ -23,6 +24,7 @@ export default function Browser_TopBar({webview, isDomReady, runningCard, setCus
         webuiAddress={runningCard.webUIAddress}
       />
       <AddressInput runningCard={runningCard} setCustomAddress={setCustomAddress} />
+      <BrowserSearch webview={webview} />
       <Browser_Zoom id={runningCard.id} />
     </>
   );
