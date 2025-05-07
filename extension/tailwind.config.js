2b613ef07684038c1b1f169498faf2026f692754
kindabrazy <kindofbrazy@gmail.com>
2025-05-07 18:23:05 +0330

    refactor(extension): update Tailwind config path for renderer files

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -3,7 +3,7 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/renderer/**/*.{html,tsx}'],
+  content: ['extension/src/renderer/**/*.{html,tsx}'],
   darkMode: 'class',
   plugins: [
     heroui({
