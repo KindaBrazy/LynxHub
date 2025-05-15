59bc09869595754c933ccf3bf2477d00251a7a91
kindabrazy <kindofbrazy@gmail.com>
2025-05-15 14:49:02 +0330

    feat(icons): add "BellDuo"

--- src/renderer/src/assets/icons/SvgIcons/SvgIcons6.tsx
+++ src/renderer/src/assets/icons/SvgIcons/SvgIcons6.tsx
@@ -55,3 +55,20 @@ export function FilterDuo_Icon(props: SvgProps): ReactElement {
     </svg>
   );
 }
+
+export function BellDuo_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} height="1rem" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d={
+          'M18.75 9v.704c0 .845.24 1.671.692 2.374l1.108 1.723c1.011 1.574.239 3.713-1.52 4' +
+          '.21a25.8 25.8 0 0 1-14.06 0c-1.759-.497-2.531-2.636-1.52-4.21l1.108-1.723a4.4 4.4' +
+          ' 0 0 0 .693-2.374V9c0-3.866 3.022-7 6.749-7s6.75 3.134 6.75 7'
+        }
+        opacity="0.5"
+        fill="currentColor"
+      />
+      <path fill="currentColor" d="M7.243 18.545a5.002 5.002 0 0 0 9.513 0c-3.145.59-6.367.59-9.513 0" />
+    </svg>
+  );
+}
