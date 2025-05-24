449169febfaf729d48a750c9932747cf82cad6a6
kindabrazy <kindofbrazy@gmail.com>
2025-05-24 14:34:51 +0330

    feat(launch_config): add animation to custom terminal command items

--- src/renderer/src/App/Components/Modals/LaunchConfig/TerminalCommand-Item.tsx
+++ src/renderer/src/App/Components/Modals/LaunchConfig/TerminalCommand-Item.tsx
@@ -1,5 +1,6 @@
 import {Card} from '@heroui/card';
 import {Button, Input} from 'antd';
+import {motion} from 'framer-motion';
 import {ChangeEvent, useCallback, useState} from 'react';
 
 import {Close_Icon} from '../../../../assets/icons/SvgIcons/SvgIcons';
@@ -31,28 +32,32 @@ export default function TerminalCommandItem({defaultText, editCommand, focus, in
   }, []);
 
   return (
-    <Card
-      className={
-        'flex h-[38px] flex-row items-center justify-center !transition-opacity' +
-        ' !duration-300 hover:opacity-80 animate-appearance-in'
-      }
-      shadow="none">
-      <Input
-        addonAfter={
-          <Button type="text" size="small" onClick={remove} icon={<Close_Icon />} className="cursor-default" danger />
+    <motion.div
+      animate={{opacity: 1, scale: 1, y: 0}}
+      exit={{opacity: 0, scale: 0.95, y: -20}}
+      initial={{opacity: 0, scale: 0.95, y: 20}}>
+      <Card
+        className={
+          'flex h-[38px] flex-row items-center justify-center !transition-opacity !duration-300 hover:opacity-80'
         }
-        width={90}
-        size="large"
-        onBlur={onBlur}
-        autoFocus={focus}
-        spellCheck="false"
-        onChange={onChange}
-        variant="borderless"
-        addonBefore={index + 1}
-        onPressEnter={editHandle}
-        defaultValue={defaultText}
-        classNames={{input: `font-JetBrainsMono text-sm`}}
-      />
-    </Card>
+        shadow="none">
+        <Input
+          addonAfter={
+            <Button type="text" size="small" onClick={remove} icon={<Close_Icon />} className="cursor-default" danger />
+          }
+          width={90}
+          size="large"
+          onBlur={onBlur}
+          autoFocus={focus}
+          spellCheck="false"
+          onChange={onChange}
+          variant="borderless"
+          addonBefore={index + 1}
+          onPressEnter={editHandle}
+          defaultValue={defaultText}
+          classNames={{input: `font-JetBrainsMono text-sm`}}
+        />
+      </Card>
+    </motion.div>
   );
 }
