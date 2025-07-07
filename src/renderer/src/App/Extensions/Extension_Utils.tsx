c8988079e93fe96d016dfe9a58603d9981340d05
kindabrazy <kindofbrazy@gmail.com>
2025-07-07 15:17:59 +0330

    refactor(extensions_ui): extract and reuse extension event utilities in install UI

---
+++ src/renderer/src/App/Extensions/Extension_Utils.tsx
@@ -0,0 +1,52 @@
+import {isArray} from 'lodash';
+import {FC} from 'react';
+
+import {extensionRendererApi} from './ExtensionLoader';
+
+export const eventUtil_TerminalPreCommands = (id: string, job: (preCommands: string[]) => void) => {
+  const preCommands: string[] = [];
+  let doneAdd: number = 0;
+
+  const listenerCount = extensionRendererApi.events.getListenerCount('card_install_command_before_terminal_action');
+  if (listenerCount > 0) {
+    extensionRendererApi.events.emit('card_install_command_before_terminal_action', {
+      id,
+      addCommand: commands => {
+        if (isArray(commands)) {
+          preCommands.push(...commands);
+        } else {
+          preCommands.push(commands);
+        }
+        doneAdd += 1;
+
+        if (doneAdd === listenerCount) {
+          job(preCommands);
+        }
+      },
+    });
+  } else {
+    job(preCommands);
+  }
+};
+
+export const eventUtil_collectUserInputs = (id: string, job: (elements: FC[]) => void) => {
+  const elements: FC[] = [];
+  let doneAdd: number = 0;
+
+  const listenerCount = extensionRendererApi.events.getListenerCount('card_collect_user_input');
+  if (listenerCount > 0) {
+    extensionRendererApi.events.emit('card_collect_user_input', {
+      id,
+      addElements: element => {
+        elements.push(...element);
+        doneAdd += 1;
+
+        if (doneAdd === listenerCount) {
+          job(elements);
+        }
+      },
+    });
+  } else {
+    job(elements);
+  }
+};
