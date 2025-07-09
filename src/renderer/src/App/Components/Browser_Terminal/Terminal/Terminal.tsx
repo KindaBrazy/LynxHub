b6884692bb012ccf3990b9a23825317400f72154
kindabrazy <kindofbrazy@gmail.com>
2025-07-09 14:45:29 +0330

    feat(install_modal): add the missing 'extensionUserInput' property to the initial modal state

--- src/renderer/src/App/Components/Browser_Terminal/Terminal/Terminal.tsx
+++ src/renderer/src/App/Components/Browser_Terminal/Terminal/Terminal.tsx
@@ -220,8 +220,6 @@ export default function Terminal({runningCard, setTerminalContent}: Props) {
 
         if (isSameSize) return;
 
-        console.log('Resizing', prevSize, size);
-
         rendererIpc.pty.resize(id, size.cols, size.rows);
         prevSize = size;
       });
