fbd6e1aa71cd084df33bdb5001e5fab788efc3fe
kindabrazy <kindofbrazy@gmail.com>
2025-07-30 15:57:39 +0330

    feat(BrowserDownloadManager): add support for download pause, resume, and cancel operations

--- src/cross/DownloadManagerTypes.ts
+++ src/cross/DownloadManagerTypes.ts
@@ -1,4 +1,5 @@
 export type DownloadProgress = {
+  name: string;
   totalBytes: number;
   receivedBytes: number;
   percent: number;
@@ -6,8 +7,15 @@ export type DownloadProgress = {
   etaSecond: number;
 };
 
+export type DownloadStartInfo = {name: string; startTime: number; url: string; totalBytes: number};
+export type DownloadDoneInfo = {name: string; state: 'completed' | 'cancelled' | 'interrupted'};
+
 export const browserDownloadChannels = {
   onDone: 'browserDL:on-done',
   onProgress: 'browserDL:on-progress',
   onDlStart: 'browserDL:on-dl-start',
+
+  cancel: 'browserDL:cancel',
+  pause: 'browserDL:pause',
+  resume: 'browserDL:resume',
 };
