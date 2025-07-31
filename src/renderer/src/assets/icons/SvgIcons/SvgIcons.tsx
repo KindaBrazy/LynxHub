7531b0112da62912c589cf28e63345e3a646c1bf
kindabrazy <kindofbrazy@gmail.com>
2025-07-31 15:35:45 +0330

    feat(Icons): add Pause_Icon component to SvgIcons

--- src/renderer/src/assets/icons/SvgIcons/SvgIcons.tsx
+++ src/renderer/src/assets/icons/SvgIcons/SvgIcons.tsx
@@ -1234,3 +1234,13 @@ export function DownloadDuo_Icon(props: SvgProps): ReactElement {
     </svg>
   );
 }
+
+export function Pause_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fill="currentColor"
+        d="M2 6c0-1.886 0-2.828.586-3.414S4.114 2 6 2s2.828 0 3.414.586S10 4.114 10 6v12c0 1.886 0 2.828-.586 3.414S7.886 22 6 22s-2.828 0-3.414-.586S2 19.886 2 18zm12 0c0-1.886 0-2.828.586-3.414S16.114 2 18 2s2.828 0 3.414.586S22 4.114 22 6v12c0 1.886 0 2.828-.586 3.414S19.886 22 18 22s-2.828 0-3.414-.586S14 19.886 14 18z"></path>
+    </svg>
+  );
+}
