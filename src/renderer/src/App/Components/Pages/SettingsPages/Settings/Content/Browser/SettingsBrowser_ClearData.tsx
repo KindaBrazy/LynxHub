b7c7635befe8aba0990c3104dd2548ce4cf3e65f
kindabrazy <kindofbrazy@gmail.com>
2025-08-10 11:01:53 +0330

    fix(settings_browser): reset selected data state after clearing

--- src/renderer/src/App/Components/Pages/SettingsPages/Settings/Content/Browser/SettingsBrowser_ClearData.tsx
+++ src/renderer/src/App/Components/Pages/SettingsPages/Settings/Content/Browser/SettingsBrowser_ClearData.tsx
@@ -33,6 +33,7 @@ export default function SettingsBrowser_ClearData() {
     setTimeout(() => {
       setIsLoading(false);
       lynxTopToast(dispatch).success('Selected data cleared successfully!');
+      setSelected([]);
     }, 300);
   }, [selected, dispatch]);
 
