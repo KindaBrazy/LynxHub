bc734b55d597e5211286c901b083600c30b82ddd
kindabrazy <kindofbrazy@gmail.com>
2025-08-24 19:36:43 +0330

    feat(share_screen): implement dark mode support and enhance thumbnail animations

--- src/renderer/share_screen/main.tsx
+++ src/renderer/share_screen/main.tsx
@@ -2,6 +2,18 @@ import '../cross_styles.css';
 
 import {createRoot} from 'react-dom/client';
 
+import rendererIpc from '../src/App/RendererIpc';
 import App from './ScreenShare';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+const {darkMode} = await rendererIpc.storage.get('app');
+const systemDarkMode = await rendererIpc.win.getSystemDarkMode();
+
+let isDarkMode = true;
+
+if (darkMode === 'system') {
+  isDarkMode = systemDarkMode === 'dark';
+} else {
+  isDarkMode = darkMode === 'dark';
+}
+
+createRoot(document.getElementById('root') as HTMLElement).render(<App isDarkMode={isDarkMode} />);
