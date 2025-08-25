8423f0e46ba5f5c440ec528364c2108cf0305fd1
kindabrazy <kindofbrazy@gmail.com>
2025-08-25 13:05:38 +0330

    refactor(cards): simplify `LynxCardBody` structure with cleaner JSX and conditional handling

--- src/renderer/src/App/Components/Cards/Card/LynxCard-Body.tsx
+++ src/renderer/src/App/Components/Cards/Card/LynxCard-Body.tsx
@@ -12,46 +12,44 @@ const LynxCardBody = observer(() => {
   const {repoUrl, installed} = useCardData();
   const repoDetails = useRepoDetails(repoUrl);
 
-  return (
-    repoDetails && (
-      <>
-        <CardBody className="flex flex-row items-center justify-center gap-x-2 bg-[#f7f7f7] text-xs dark:bg-[#292929]">
-          <div className="flex flex-col items-center justify-center hover:opacity-50 transition duration-300">
-            <span className="font-bold">{formatNumber(repoDetails.stars)}</span>
-            <div className="flex flex-row items-center">
-              <Star_Icon className="fill-yellow-500 mx-1" />
-              <span>Stars</span>
-            </div>
-          </div>
+  if (!repoDetails) return null;
 
+  return (
+    <CardBody className="flex flex-row items-center justify-center gap-x-2 bg-[#f7f7f7] text-xs dark:bg-[#292929]">
+      <div className="flex flex-col items-center justify-center hover:opacity-50 transition duration-300">
+        <span className="font-bold">{formatNumber(repoDetails.stars)}</span>
+        <div className="flex flex-row items-center">
+          <Star_Icon className="fill-yellow-500 mx-1" />
+          <span>Stars</span>
+        </div>
+      </div>
+
+      <Divider type="vertical" />
+
+      <div className="flex flex-col items-center justify-center hover:opacity-50 transition duration-300">
+        <span className="font-bold">{formatNumber(repoDetails.forks)}</span>
+
+        <div className="flex flex-row items-center">
+          <Fork_Icon className="mx-1" />
+          <span>Forks</span>
+        </div>
+      </div>
+
+      {!installed && (
+        <>
           <Divider type="vertical" />
 
           <div className="flex flex-col items-center justify-center hover:opacity-50 transition duration-300">
-            <span className="font-bold">{formatNumber(repoDetails.forks)}</span>
+            <span className="font-bold">{formatSizeKB(repoDetails.size)}</span>
 
             <div className="flex flex-row items-center">
-              <Fork_Icon className="mx-1" />
-              <span>Forks</span>
+              <Download_Icon className="mx-1" />
+              <span>Size</span>
             </div>
           </div>
-
-          {!installed && (
-            <>
-              <Divider type="vertical" />
-
-              <div className="flex flex-col items-center justify-center hover:opacity-50 transition duration-300">
-                <span className="font-bold">{formatSizeKB(repoDetails.size)}</span>
-
-                <div className="flex flex-row items-center">
-                  <Download_Icon className="mx-1" />
-                  <span>Size</span>
-                </div>
-              </div>
-            </>
-          )}
-        </CardBody>
-      </>
-    )
+        </>
+      )}
+    </CardBody>
   );
 });
 export default LynxCardBody;
