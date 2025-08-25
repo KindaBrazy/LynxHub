c132c886c8cb0176a848bf3a002f80bb52d0e775
kindabrazy <kindofbrazy@gmail.com>
2025-08-25 10:45:39 +0330

    refactor(launch_config): replace custom copy-to-clipboard logic with reusable `CopyClipboard` component and clean up unused imports

--- src/renderer/src/App/Components/Modals/LaunchConfig/Arguments/PreviewArguments.tsx
+++ src/renderer/src/App/Components/Modals/LaunchConfig/Arguments/PreviewArguments.tsx
@@ -1,13 +1,7 @@
-import {Button} from '@heroui/react';
 import {Card, Empty, Typography} from 'antd';
 import {isEmpty} from 'lodash';
-import {useCallback} from 'react';
-import {useDispatch} from 'react-redux';
 
-import {Copy_Icon} from '../../../../../assets/icons/SvgIcons/SvgIcons';
-import {AppDispatch} from '../../../../Redux/Store';
-import {lynxTopToast} from '../../../../Utils/UtilHooks';
-import LynxTooltip from '../../../Reusable/LynxTooltip';
+import CopyClipboard from '../../../Reusable/CopyClipboard';
 import LaunchConfigSection from '../LaunchConfig-Section';
 
 const {Paragraph} = Typography;
@@ -16,40 +10,21 @@ type Props = {text: string};
 
 /** Show arguments as text for preview and clipboard copying */
 export default function PreviewArguments({text}: Props) {
-  const dispatch = useDispatch<AppDispatch>();
-
-  const onCopy = useCallback(async () => {
-    try {
-      await navigator.clipboard.writeText(text);
-      lynxTopToast(dispatch).success('Copied to clipboard successfully.');
-    } catch (_) {
-      lynxTopToast(dispatch).error('Error: Unable to copy to clipboard.');
-    }
-  }, [text]);
-
   return (
     <LaunchConfigSection
-      customButton={
-        <LynxTooltip content="Copy Preview to Clipboard" isEssential>
-          <Button size="sm" variant="light" onPress={onCopy} className="cursor-default" isIconOnly>
-            <Copy_Icon className="size-3.5" />
-          </Button>
-        </LynxTooltip>
-      }
       title="Preview"
-      description="Preview of the final result to be saved">
+      description="Preview of the final result to be saved"
+      customButton={<CopyClipboard contentToCopy={text} tooltipTitle="Copy Preview to Clipboard" showTooltip />}>
       {isEmpty(text) ? (
         <Empty className="m-0" image={Empty.PRESENTED_IMAGE_SIMPLE} description="No preview available to display" />
       ) : (
-        <>
-          <Card variant="borderless" className={`cursor-default dark:bg-black/40`} hoverable>
-            <Paragraph
-              ellipsis={{expandable: 'collapsible', rows: 2}}
-              className="size-full whitespace-pre-line !font-JetBrainsMono">
-              {text}
-            </Paragraph>
-          </Card>
-        </>
+        <Card variant="borderless" className={`cursor-default dark:bg-black/40`} hoverable>
+          <Paragraph
+            ellipsis={{expandable: 'collapsible', rows: 2}}
+            className="size-full whitespace-pre-line !font-JetBrainsMono">
+            {text}
+          </Paragraph>
+        </Card>
       )}
     </LaunchConfigSection>
   );
