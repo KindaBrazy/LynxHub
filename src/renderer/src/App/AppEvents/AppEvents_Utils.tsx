1a41a8f3a3df0a434fa3448d4eacd48ccdec9160
kindabrazy <kindofbrazy@gmail.com>
2025-08-31 16:18:22 +0330

    refactor(utils): simplify EA check logic with Promise.all for enhanced readability

--- src/renderer/src/App/AppEvents/AppEvents_Utils.tsx
+++ src/renderer/src/App/AppEvents/AppEvents_Utils.tsx
@@ -5,44 +5,34 @@ import rendererIpc from '../RendererIpc';
 import {isLinuxPortable} from '../Utils/UtilHooks';
 
 export const checkEARepos = (isEA: boolean, isInsider: boolean) => {
-  rendererIpc.extension
-    .checkEa(isEA, isInsider)
-    .then(eEA => {
-      rendererIpc.module
-        .checkEa(isEA, isInsider)
-        .then(mEA => {
-          if (eEA || mEA) {
-            notification.info({
-              duration: 0,
-              key: 'need_restart',
-              message: 'Update Stage Changed',
-              closable: false,
-              description:
-                `The extensions and modules updates has been changed to ` +
-                `${isEA ? 'Early Access' : isInsider ? 'Insider' : 'Normal'}` +
-                ` stage. Please restart for changes to be applied.`,
-              actions: (
-                <div className="flex flex-row gap-x-2">
-                  <Button
-                    size="sm"
-                    variant="light"
-                    color="success"
-                    onPress={() => rendererIpc.win.changeWinState(isLinuxPortable ? 'close' : 'restart')}>
-                    {isLinuxPortable ? 'Exit' : 'Restart'}
-                  </Button>
-                  <Button
-                    size="sm"
-                    variant="light"
-                    color="warning"
-                    onPress={() => notification.destroy('need_restart')}>
-                    Do It Later
-                  </Button>
-                </div>
-              ),
-            });
-          }
-        })
-        .catch(console.error);
+  Promise.all([rendererIpc.extension.checkEa(isEA, isInsider), rendererIpc.module.checkEa(isEA, isInsider)])
+    .then(([extension, module]) => {
+      if (extension || module) {
+        notification.info({
+          duration: 0,
+          key: 'need_restart',
+          message: 'Update Stage Changed',
+          closable: false,
+          description:
+            `The extensions and modules updates has been changed to ` +
+            `${isEA ? 'Early Access' : isInsider ? 'Insider' : 'Normal'}` +
+            ` stage. Please restart for changes to be applied.`,
+          actions: (
+            <div className="flex flex-row gap-x-2">
+              <Button
+                size="sm"
+                variant="light"
+                color="success"
+                onPress={() => rendererIpc.win.changeWinState(isLinuxPortable ? 'close' : 'restart')}>
+                {isLinuxPortable ? 'Exit' : 'Restart'}
+              </Button>
+              <Button size="sm" variant="light" color="warning" onPress={() => notification.destroy('need_restart')}>
+                Do It Later
+              </Button>
+            </div>
+          ),
+        });
+      }
     })
     .catch(console.error);
 };
