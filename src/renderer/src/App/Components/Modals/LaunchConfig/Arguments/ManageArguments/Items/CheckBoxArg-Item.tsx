688341ab65f9ca1345aae94d4ae34db813c4d2ab
kindabrazy <kindofbrazy@gmail.com>
2025-09-23 14:59:49 +0330

    refactor(arguments): rename CheckBox_Icon to CheckSquareDuo_Icon and update usage in CheckBoxArgItem component

--- src/renderer/src/App/Components/Modals/LaunchConfig/Arguments/ManageArguments/Items/CheckBoxArg-Item.tsx
+++ src/renderer/src/App/Components/Modals/LaunchConfig/Arguments/ManageArguments/Items/CheckBoxArg-Item.tsx
@@ -1,9 +1,16 @@
 import {ChosenArgument} from '../../../../../../../../../cross/CrossTypes';
-import {CheckBox_Icon} from '../../../../../../../assets/icons/SvgIcons/SvgIcons';
+import {CheckSquareDuo_Icon} from '../../../../../../../assets/icons/SvgIcons/SvgIcons';
 import ArgumentItemBase from './Argument-Item-Base';
 
 type Props = {argument: ChosenArgument; removeArg: () => void; changeValue: (value: any) => void; id: string};
 
 export default function CheckBoxArgItem({argument, removeArg, id}: Props) {
-  return <ArgumentItemBase id={id} name={argument.name} removeArg={removeArg} icon={<CheckBox_Icon />} />;
+  return (
+    <ArgumentItemBase
+      id={id}
+      name={argument.name}
+      removeArg={removeArg}
+      icon={<CheckSquareDuo_Icon className="size-4" />}
+    />
+  );
 }
