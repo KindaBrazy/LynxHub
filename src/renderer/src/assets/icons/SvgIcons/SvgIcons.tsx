f22d2cd613bc64b664ede77f071cf9b5affa8f4c
kindabrazy <kindofbrazy@gmail.com>
2025-09-23 14:53:35 +0330

    feat(icons): add `Grip_Icon` component to SvgIcons module

--- src/renderer/src/assets/icons/SvgIcons/SvgIcons.tsx
+++ src/renderer/src/assets/icons/SvgIcons/SvgIcons.tsx
@@ -1244,3 +1244,15 @@ export function Pause_Icon(props: SvgProps): ReactElement {
     </svg>
   );
 }
+
+export function Grip_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        fill="currentColor"
+        d="M7 3a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0M5.5 9.5a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3m5 0a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3m0-5a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3M7 13a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0m3.5 1.5a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3"></path>
+    </svg>
+  );
+}
