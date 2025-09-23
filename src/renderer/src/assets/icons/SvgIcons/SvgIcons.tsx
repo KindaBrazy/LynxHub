b3fcc6ad6ae79ccb704b1231b984baa7aa482bb1
kindabrazy <kindofbrazy@gmail.com>
2025-09-23 14:59:49 +0330

    refactor(arguments): rename CheckBox_Icon to CheckSquareDuo_Icon and update usage in CheckBoxArgItem component

--- src/renderer/src/assets/icons/SvgIcons/SvgIcons.tsx
+++ src/renderer/src/assets/icons/SvgIcons/SvgIcons.tsx
@@ -35,15 +35,16 @@ export function AudioGeneration_Icon(props: SvgProps): ReactElement {
   );
 }
 
-export function CheckBox_Icon(props: SvgProps): ReactElement {
+export function CheckSquareDuo_Icon(props: SvgProps): ReactElement {
   return (
-    <svg {...props} height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <svg {...props} width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
+        opacity={0.5}
         fill="currentColor"
-        d="M12 22c-4.714 0-7.071 0-8.536-1.465C2 19.072 2 16.714 2 12s0-7.071 1.464-8.536C4.93 2 7.286 2 12 2c4.714 0 7.071 0 8.535 1.464C22 4.93 22 7.286 22 12c0 4.714 0 7.071-1.465 8.535C19.072 22 16.714 22 12 22m4.03-13.03a.75.75 0 0 1 0 1.06l-5 5a.75.75 0 0 1-1.06 0l-2-2a.75.75 0 1 1 1.06-1.06l1.47 1.47l4.47-4.47a.75.75 0 0 1 1.06 0"
-      />
+        d="M12 22c-4.714 0-7.071 0-8.536-1.465C2 19.072 2 16.714 2 12s0-7.071 1.464-8.536C4.93 2 7.286 2 12 2s7.071 0 8.535 1.464C22 4.93 22 7.286 22 12s0 7.071-1.465 8.535C19.072 22 16.714 22 12 22"></path>
+      <path
+        fill="currentColor"
+        d="M16.03 8.97a.75.75 0 0 1 0 1.06l-5 5a.75.75 0 0 1-1.06 0l-2-2a.75.75 0 1 1 1.06-1.06l1.47 1.47l4.47-4.47a.75.75 0 0 1 1.06 0"></path>
     </svg>
   );
 }
