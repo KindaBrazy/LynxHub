cf658c9a574f83d24c0b33e5da2de88d87b1d86b
kindabrazy <kindofbrazy@gmail.com>
2025-09-23 15:00:15 +0330

    feat(icons): add FileDuo_Icon component to SvgIcons for new file representation

--- src/renderer/src/assets/icons/SvgIcons/SvgIcons.tsx
+++ src/renderer/src/assets/icons/SvgIcons/SvgIcons.tsx
@@ -193,6 +193,22 @@ export function File_Icon(props: SvgProps): ReactElement {
   );
 }
 
+export function FileDuo_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        opacity={0.5}
+        fillRule="evenodd"
+        clipRule="evenodd"
+        fill="currentColor"
+        d="M14 22h-4c-3.771 0-5.657 0-6.828-1.172S2 17.771 2 14v-4c0-3.771 0-5.657 1.172-6.828S6.239 2 10.03 2c.606 0 1.091 0 1.5.017q-.02.12-.02.244l-.01 2.834c0 1.097 0 2.067.105 2.848c.114.847.375 1.694 1.067 2.386c.69.69 1.538.952 2.385 1.066c.781.105 1.751.105 2.848.105h4.052c.043.534.043 1.19.043 2.063V14c0 3.771 0 5.657-1.172 6.828S17.771 22 14 22"></path>
+      <path
+        fill="currentColor"
+        d="m11.51 2.26l-.01 2.835c0 1.097 0 2.066.105 2.848c.114.847.375 1.694 1.067 2.385c.69.691 1.538.953 2.385 1.067c.781.105 1.751.105 2.848.105h4.052q.02.232.028.5H22c0-.268 0-.402-.01-.56a5.3 5.3 0 0 0-.958-2.641c-.094-.128-.158-.204-.285-.357C19.954 7.494 18.91 6.312 18 5.5c-.81-.724-1.921-1.515-2.89-2.161c-.832-.556-1.248-.834-1.819-1.04a6 6 0 0 0-.506-.154c-.384-.095-.758-.128-1.285-.14z"></path>
+    </svg>
+  );
+}
+
 export function Filter_Icon(props: SvgProps): ReactElement {
   return (
     <svg {...props} height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
