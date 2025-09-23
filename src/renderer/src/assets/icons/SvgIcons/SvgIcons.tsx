a89a8e31e53251e750d6f586c39b3dd242443bc2
kindabrazy <kindofbrazy@gmail.com>
2025-09-23 15:01:16 +0330

    feat(icons): add ListCheckDuo_Icon component and update DropdownArgItem to use it

--- src/renderer/src/assets/icons/SvgIcons/SvgIcons.tsx
+++ src/renderer/src/assets/icons/SvgIcons/SvgIcons.tsx
@@ -345,6 +345,22 @@ export function ListCheck_Icon(props: SvgProps): ReactElement {
   );
 }
 
+export function ListCheckDuo_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        opacity={0.5}
+        fillRule="evenodd"
+        clipRule="evenodd"
+        fill="currentColor"
+        d="M3 6.75A.75.75 0 0 1 3.75 6h17a.75.75 0 0 1 0 1.5h-17A.75.75 0 0 1 3 6.75m0 5a.75.75 0 0 1 .75-.75h7a.75.75 0 0 1 0 1.5h-7a.75.75 0 0 1-.75-.75m0 5a.75.75 0 0 1 .75-.75h7a.75.75 0 0 1 0 1.5h-7a.75.75 0 0 1-.75-.75"></path>
+      <path
+        fill="currentColor"
+        d="M21.211 11.159a.75.75 0 0 1 .13 1.052l-3.9 5a.75.75 0 0 1-1.165.021l-2.1-2.5a.75.75 0 0 1 1.148-.964l1.504 1.79l3.33-4.27a.75.75 0 0 1 1.053-.13"></path>
+    </svg>
+  );
+}
+
 export function Maximize_Icon(props: SvgProps): ReactElement {
   return (
     <svg {...props} height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
