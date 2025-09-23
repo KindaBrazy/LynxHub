3ac96873404ec37353664d1d9aa17c088354cae7
kindabrazy <kindofbrazy@gmail.com>
2025-09-23 14:55:42 +0330

    refactor(modals): update "LaunchConfig-Section" to use "Card" from "@heroui/react"

--- src/renderer/src/App/Components/Modals/LaunchConfig/LaunchConfig-Section.tsx
+++ src/renderer/src/App/Components/Modals/LaunchConfig/LaunchConfig-Section.tsx
@@ -1,5 +1,4 @@
-import {Button} from '@heroui/react';
-import {Card} from 'antd';
+import {Button, Card, CardBody, CardHeader} from '@heroui/react';
 import {isEmpty, isString} from 'lodash';
 import {ReactNode} from 'react';
 
@@ -25,25 +24,21 @@ export default function LaunchConfigSection({
   customButton,
 }: Props) {
   return (
-    <Card
-      title={
-        <div className="flex w-full flex-col py-3">
-          <div className="flex w-full flex-row items-center justify-between">
-            {isString(title) ? <span>{title}</span> : title}
-            {customButton || (
-              <LynxTooltip content={addTooltipTitle} isEssential>
-                <Button size="sm" variant="light" onPress={onAddPress} className="cursor-default" isIconOnly>
-                  <Add_Icon />
-                </Button>
-              </LynxTooltip>
-            )}
-          </div>
-          {!isEmpty(description) && <span className="text-sm font-normal text-foreground-500">{description}</span>}
+    <Card className="bg-foreground-100">
+      <CardHeader className="px-4 flex-col items-start">
+        <div className="flex w-full flex-row items-center justify-between">
+          {isString(title) ? <span>{title}</span> : title}
+          {customButton || (
+            <LynxTooltip content={addTooltipTitle} isEssential>
+              <Button size="sm" variant="light" onPress={onAddPress} isIconOnly>
+                <Add_Icon />
+              </Button>
+            </LynxTooltip>
+          )}
         </div>
-      }
-      variant="borderless"
-      className="border-2 border-foreground/5 bg-default-100">
-      {children}
+        {!isEmpty(description) && <span className="text-sm font-normal text-foreground-500">{description}</span>}
+      </CardHeader>
+      <CardBody>{children}</CardBody>
     </Card>
   );
 }
