b75240b20a4f842778538875c0c984b94c6f57ed
kindabrazy <kindofbrazy@gmail.com>
2025-09-23 14:57:29 +0330

    refactor(arguments): simplify CheckBoxArgItem by reusing ArgumentItemBase component

--- src/renderer/src/App/Components/Modals/LaunchConfig/Arguments/ManageArguments/Items/CheckBoxArg-Item.tsx
+++ src/renderer/src/App/Components/Modals/LaunchConfig/Arguments/ManageArguments/Items/CheckBoxArg-Item.tsx
@@ -1,39 +1,9 @@
-import {Button} from '@heroui/react';
-import {Card, Tooltip, Typography} from 'antd';
-import {useMemo} from 'react';
-
 import {ChosenArgument} from '../../../../../../../../../cross/CrossTypes';
-import {getArgumentDescription} from '../../../../../../../../../cross/GetArgumentsData';
-import {CheckBox_Icon, Close_Icon} from '../../../../../../../assets/icons/SvgIcons/SvgIcons';
-import {useGetArgumentsByID} from '../../../../../../Modules/ModuleLoader';
+import {CheckBox_Icon} from '../../../../../../../assets/icons/SvgIcons/SvgIcons';
+import ArgumentItemBase from './Argument-Item-Base';
 
-const {Text} = Typography;
 type Props = {argument: ChosenArgument; removeArg: () => void; changeValue: (value: any) => void; id: string};
 
 export default function CheckBoxArgItem({argument, removeArg, id}: Props) {
-  const cardArgument = useGetArgumentsByID(id);
-
-  const tooltipText = useMemo(() => getArgumentDescription(argument.name, cardArgument), [argument, cardArgument]);
-
-  return (
-    <Tooltip title={tooltipText} mouseEnterDelay={0.5} rootClassName="max-w-[65%] whitespace-pre-line">
-      <Card
-        variant="borderless"
-        className="cursor-default dark:bg-black/40"
-        classNames={{body: 'flex-row items-center flex'}}
-        hoverable>
-        <CheckBox_Icon className="text-success absolute left-3" />
-        <Text className="absolute left-8 right-12 font-JetBrainsMono text-success">{argument.name}</Text>
-        <Button
-          size="sm"
-          color="danger"
-          variant="light"
-          onPress={removeArg}
-          className="absolute right-3 cursor-default"
-          isIconOnly>
-          <Close_Icon />
-        </Button>
-      </Card>
-    </Tooltip>
-  );
+  return <ArgumentItemBase id={id} name={argument.name} removeArg={removeArg} icon={<CheckBox_Icon />} />;
 }
