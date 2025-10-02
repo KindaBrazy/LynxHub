bb0f4fd0f91d197c48d98871c1e2ca1dce68794e
kindabrazy <kindofbrazy@gmail.com>
2025-10-02 10:37:46 +0330

    refactor(config): migrate Prettier configuration to TypeScript format

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import {type Config} from 'prettier';
+
+const config: Config = {
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: false,
+  bracketSameLine: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+};
+
+export default config;
