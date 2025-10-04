7f4c3df210af5ede961b470d8c8324024742fdac
kindabrazy <kindofbrazy@gmail.com>
2025-10-04 11:45:53 +0330

    refactor(plugins): extract target version logic to reusable utility

--- src/main/Managements/Plugin/PluginUtils.ts
+++ src/main/Managements/Plugin/PluginUtils.ts
@@ -1,53 +1,7 @@
 import {SubscribeStages} from '../../../cross/CrossTypes';
-import {PluginUpdateList, PluginVersions, VersionItem} from '../../../cross/plugin/PluginTypes';
+import {getTargetCommit, getTargetVersion} from '../../../cross/plugin/CrossPluginUtils';
+import {PluginUpdateList} from '../../../cross/plugin/PluginTypes';
 import {staticManager} from '../../index';
-
-export function getTargetVersion(versions: PluginVersions, stage: SubscribeStages) {
-  const findVersionByStage = (requiredStage: SubscribeStages): VersionItem | undefined => {
-    return versions.find(v => v.stage.includes(requiredStage));
-  };
-
-  let versionItem: VersionItem | undefined = undefined;
-
-  switch (stage) {
-    case 'insider': {
-      versionItem = findVersionByStage('insider');
-
-      if (!versionItem) {
-        versionItem = findVersionByStage('early_access');
-      }
-
-      if (!versionItem) {
-        versionItem = findVersionByStage('public');
-      }
-      break;
-    }
-
-    case 'early_access': {
-      versionItem = findVersionByStage('early_access');
-
-      if (!versionItem) {
-        versionItem = findVersionByStage('public');
-      }
-      break;
-    }
-
-    case 'public': {
-      versionItem = findVersionByStage('public');
-      break;
-    }
-  }
-
-  if (versionItem) {
-    return versionItem;
-  } else {
-    return versions[0];
-  }
-}
-
-export function getTargetCommit(versions: PluginVersions, stage: SubscribeStages) {
-  return getTargetVersion(versions, stage).commit;
-}
 
 export async function getVersionByCommit(id: string, commit: string) {
   const {versions} = await staticManager.getPluginVersioningById(id);
