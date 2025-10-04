196b7a0abf16c3b209e5c100ab455b0824924b47
kindabrazy <kindofbrazy@gmail.com>
2025-10-04 11:45:53 +0330

    refactor(plugins): extract target version logic to reusable utility

---
+++ src/cross/plugin/CrossPluginUtils.ts
@@ -0,0 +1,49 @@
+import {SubscribeStages} from '../CrossTypes';
+import {PluginVersions, VersionItem} from './PluginTypes';
+
+export function getTargetVersion(versions: PluginVersions, stage: SubscribeStages) {
+  const findVersionByStage = (requiredStage: SubscribeStages): VersionItem | undefined => {
+    return versions.find(v => v.stage.includes(requiredStage));
+  };
+
+  let versionItem: VersionItem | undefined = undefined;
+
+  switch (stage) {
+    case 'insider': {
+      versionItem = findVersionByStage('insider');
+
+      if (!versionItem) {
+        versionItem = findVersionByStage('early_access');
+      }
+
+      if (!versionItem) {
+        versionItem = findVersionByStage('public');
+      }
+      break;
+    }
+
+    case 'early_access': {
+      versionItem = findVersionByStage('early_access');
+
+      if (!versionItem) {
+        versionItem = findVersionByStage('public');
+      }
+      break;
+    }
+
+    case 'public': {
+      versionItem = findVersionByStage('public');
+      break;
+    }
+  }
+
+  if (versionItem) {
+    return versionItem;
+  } else {
+    return versions[0];
+  }
+}
+
+export function getTargetCommit(versions: PluginVersions, stage: SubscribeStages) {
+  return getTargetVersion(versions, stage).commit;
+}
