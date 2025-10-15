237b4c7faae2e8335f29f423de4be6051f8a8c4f
kindabrazy <kindofbrazy@gmail.com>
2025-10-15 20:55:31 +0330

    refactor(loading): extract reusable components for loading screens and update text style

--- src/renderer/loading/Loadings/RippleLoading.tsx
+++ src/renderer/loading/Loadings/RippleLoading.tsx
@@ -1,17 +1,12 @@
-import {APP_NAME} from '../../../cross/CrossConstants';
-import ShinyText from '../../src/App/Components/Reusable/ShinyText';
 import {Ripple} from '../Backgrounds/RippleBG';
+import {AppName} from './AppName';
+import Container from './Container';
 
 export default function ThreadsLoading() {
   return (
-    <div
-      className={
-        'dark absolute inset-0 flex flex-col items-center justify-center overflow-hidden draggable bg-LynxRaisinBlack'
-      }>
+    <Container>
       <Ripple numCircles={6} mainCircleSize={110} mainCircleOpacity={0.4} />
-      <div className="w-full text-center">
-        <ShinyText speed={2} text={APP_NAME} darkMode={true} className="text-[2.7rem] font-semibold tracking-tighter" />
-      </div>
-    </div>
+      <AppName className="text-center" />
+    </Container>
   );
 }
--- src/renderer/loading/Loadings/ThreadsLoading.tsx
+++ src/renderer/loading/Loadings/ThreadsLoading.tsx
@@ -1,14 +1,12 @@
-import {APP_NAME} from '../../../cross/CrossConstants';
-import ShinyText from '../../src/App/Components/Reusable/ShinyText';
 import ThreadsBG from '../Backgrounds/ThreadsBG';
+import {AppName} from './AppName';
+import Container from './Container';
 
 export default function ThreadsLoading() {
   return (
-    <div className="draggable absolute inset-0 overflow-hidden bg-LynxRaisinBlack scrollbar-hide">
+    <Container>
       <ThreadsBG color={[1, 1, 1]} />
-      <div className="absolute bottom-10 w-full text-center py-5">
-        <ShinyText speed={2} className="text-xl font-semibold" text={`Powering Up ${APP_NAME}...`} />
-      </div>
-    </div>
+      <AppName className="absolute bottom-10" />
+    </Container>
   );
 }
