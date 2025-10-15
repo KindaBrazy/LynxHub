6f6e4fe8b5dd7f0fc08655806ea220498af010b3
kindabrazy <kindofbrazy@gmail.com>
2025-10-15 21:02:37 +0330

    feat(loading): enhance loading components with dynamic text and animation

--- src/renderer/loading/Loadings/SimpleLoading.tsx
+++ src/renderer/loading/Loadings/SimpleLoading.tsx
@@ -1,12 +1,10 @@
-import {APP_NAME} from '../../../cross/CrossConstants';
-import ShinyText from '../../src/App/Components/Reusable/ShinyText';
+import {AppName} from './AppName';
+import Container from './Container';
 
 export default function SimpleLoading() {
   return (
-    <div className="draggable absolute inset-0 overflow-hidden bg-LynxRaisinBlack scrollbar-hide">
-      <div className="absolute top-1/2 -translate-y-1/2 w-full text-center py-5">
-        <ShinyText speed={2} className="text-xl font-semibold" text={`Powering Up ${APP_NAME}...`} />
-      </div>
-    </div>
+    <Container>
+      <AppName className="text-center" textClassName="!text-[3rem]" />
+    </Container>
   );
 }
