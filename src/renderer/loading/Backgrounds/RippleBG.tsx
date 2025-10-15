0d098ad943c22348373b4dd78586103567777c03
kindabrazy <kindofbrazy@gmail.com>
2025-10-15 20:40:42 +0330

    feat(loading): add ripple loading animation

---
+++ src/renderer/loading/Backgrounds/RippleBG.tsx
@@ -0,0 +1,56 @@
+import {cn} from '@heroui/react';
+import React, {ComponentPropsWithoutRef, CSSProperties} from 'react';
+
+interface RippleProps extends ComponentPropsWithoutRef<'div'> {
+  mainCircleSize?: number;
+  mainCircleOpacity?: number;
+  numCircles?: number;
+}
+export const Ripple = React.memo(function Ripple({
+  mainCircleSize = 210,
+  mainCircleOpacity = 0.24,
+  numCircles = 8,
+  className,
+  ...props
+}: RippleProps) {
+  return (
+    <div
+      className={cn(
+        'pointer-events-none absolute inset-0 select-none [mask-image:linear-gradient(to_bottom,white,transparent)]',
+        className,
+      )}
+      {...props}>
+      {Array.from({length: numCircles}, (_, i) => {
+        const size = mainCircleSize + i * 70;
+        const opacity = mainCircleOpacity - i * 0.03;
+        const animationDelay = `${i * 0.06}s`;
+        const borderStyle = 'solid';
+        return (
+          <div
+            style={
+              {
+                '--i': i,
+                width: `${size}px`,
+                height: `${size}px`,
+                opacity,
+                animationDelay,
+                borderStyle,
+                borderWidth: '1px',
+                borderColor: `var(--foreground)`,
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%) scale(1)',
+              } as CSSProperties
+            }
+            key={i}
+            className={`absolute animate-ripple rounded-full border bg-foreground/25 shadow-xl`}
+          />
+        );
+      })}
+    </div>
+  );
+});
+
+Ripple.displayName = 'Ripple';
+
+export type {RippleProps};
