4527b7fa1aeab42f509a5b2cb37db92140795937
kindabrazy <kindofbrazy@gmail.com>
2025-10-19 13:46:15 +0330

    refactor(renderer): consolidate app hooks into MainHooks component

--- src/renderer/src/App/App.tsx
+++ src/renderer/src/App/App.tsx
@@ -1,23 +1,16 @@
-import useAppEvents from './AppEvents/AppEvents';
-import useStateChange from './AppEvents/AppStates';
 import Background from './Components/Background';
 import Initializer from './Components/Initializer/Initializer';
 import MainContents from './Components/MainContents/MainContents';
 import Modals from './Components/Modals/Modals';
 import TitleBar from './Components/TitleBar/TitleBar';
 import ExtensionHooks from './ExtensionHooks';
+import AppHooks from './MainHooks';
 import UIProviders from './UIProviders';
-import {useRegisterHotkeys} from './Utils/RegisterHotkeys';
-import useHtmlAttributes from './Utils/SetHtmlAttributes';
 
 export default function App() {
-  useHtmlAttributes();
-  useRegisterHotkeys();
-  useAppEvents();
-  useStateChange();
-
   return (
     <UIProviders>
+      <AppHooks />
       <Initializer />
       <ExtensionHooks />
       <Background />
--- src/renderer/src/App/AppEvents/AppStates.ts
+++
@@ -1,5 +0,0 @@
-import {useFilterPinnedCards} from './AppStates_Hooks';
-
-export default function useStateChange() {
-  useFilterPinnedCards();
-}
