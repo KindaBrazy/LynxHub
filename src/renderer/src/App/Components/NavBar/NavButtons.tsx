daf92f60d388428635001836fb4ab1334f035812
kindabrazy <kindofbrazy@gmail.com>
2025-10-20 11:12:19 +0330

    refactor(navbar): use new navbar

--- src/renderer/src/App/Components/NavBar/NavButtons.tsx
+++ src/renderer/src/App/Components/NavBar/NavButtons.tsx
@@ -1,5 +1,5 @@
 import {isEmpty} from 'lodash';
-import {ReactNode, useMemo} from 'react';
+import {useMemo} from 'react';
 
 import {PageID, PageTitles} from '../../../../../cross/CrossConstants';
 import {
@@ -17,97 +17,67 @@ import {
 } from '../../../assets/icons/SvgIcons/SvgIcons';
 import {extensionsData} from '../../Extensions/ExtensionLoader';
 import {hasCardsByPath} from '../../Modules/ModuleLoader';
-import {usePluginsState} from '../../Redux/Reducer/PluginsReducer';
-import {useSettingsState} from '../../Redux/Reducer/SettingsReducer';
-import NavButton from './NavButton';
-
-type PagesType = {
-  path: string;
-  navButton: {
-    title: string;
-    icon: ReactNode;
-    badge?: boolean;
-  };
-};
-
-const GetPages = ({Pages}: {Pages: PagesType[]}) => {
-  const syncList = usePluginsState('syncList');
-  const appUpdateAvailable = useSettingsState('updateAvailable');
-
-  return Pages.map(page => {
-    const {navButton, path} = page;
-    const {icon, title} = navButton;
-
-    const dashboardBadge = navButton.title === 'Dashboard' && appUpdateAvailable;
-    const pluginsBadge = navButton.title === 'Plugins' && !isEmpty(syncList) ? syncList.length : false;
-
-    return (
-      <NavButton title={title} pageId={path} key={`navBtn-${path}`} badge={dashboardBadge || pluginsBadge}>
-        {icon}
-      </NavButton>
-    );
-  });
-};
+import {NavItem} from '../../Utils/Types';
+import FloatingNav from './FloatingNav';
 
 export const ContentPagesButtons = () => {
   const contentBar = useMemo(() => extensionsData.navBar.addButton.contentBar, []);
 
-  const pagesData: PagesType[] = useMemo(() => {
-    const result: PagesType[] = [
-      {navButton: {icon: <Home_Icon className="size-full" />, title: PageTitles.home}, path: PageID.home},
-    ];
+  const pagesData: NavItem[] = useMemo(() => {
+    const result: NavItem[] = [{icon: <Home_Icon className="size-full" />, title: PageTitles.home, path: PageID.home}];
 
     if (hasCardsByPath(PageID.imageGen) || hasCardsByPath('/imageGenerationPage')) {
       result.push({
-        navButton: {icon: <ImageGeneration_Icon className="size-full" />, title: PageTitles.imageGen},
+        icon: <ImageGeneration_Icon className="size-full" />,
+        title: PageTitles.imageGen,
         path: PageID.imageGen,
       });
     }
 
     if (hasCardsByPath(PageID.textGen) || hasCardsByPath('/textGenerationPage')) {
       result.push({
-        navButton: {icon: <TextGeneration_Icon className="size-full" />, title: PageTitles.textGen},
+        icon: <TextGeneration_Icon className="size-full" />,
+        title: PageTitles.textGen,
         path: PageID.textGen,
       });
     }
 
     if (hasCardsByPath(PageID.audioGen) || hasCardsByPath('/audioGenerationPage')) {
       result.push({
-        navButton: {icon: <AudioGeneration_Icon className="size-full" />, title: PageTitles.audioGen},
+        icon: <AudioGeneration_Icon className="size-full" />,
+        title: PageTitles.audioGen,
         path: PageID.audioGen,
       });
     }
 
     if (hasCardsByPath(PageID.agents)) {
       result.push({
-        navButton: {icon: <Robot_Icon className="size-full" />, title: PageTitles.agents},
+        icon: <Robot_Icon className="size-full" />,
+        title: PageTitles.agents,
         path: PageID.agents,
       });
     }
 
     if (hasCardsByPath(PageID.others)) {
       result.push({
-        navButton: {icon: <MagicStickDuo_Icon className="size-full" />, title: PageTitles.others},
+        icon: <MagicStickDuo_Icon className="size-full" />,
+        title: PageTitles.others,
         path: PageID.others,
       });
     }
 
     if (!isEmpty(extensionsData.customizePages.tools.addComponent)) {
       result.push({
-        navButton: {
-          icon: <Rocket_Icon className="size-full" />,
-          title: PageTitles.tools,
-        },
+        icon: <Rocket_Icon className="size-full" />,
+        title: PageTitles.tools,
         path: PageID.tools,
       });
     }
 
     if (!isEmpty(extensionsData.customizePages.games.addComponent)) {
       result.push({
-        navButton: {
-          icon: <GamePad_Icon className="size-full" />,
-          title: PageTitles.games,
-        },
+        icon: <GamePad_Icon className="size-full" />,
+        title: PageTitles.games,
         path: PageID.games,
       });
     }
@@ -117,16 +87,16 @@ export const ContentPagesButtons = () => {
 
   return (
     <>
-      <GetPages Pages={pagesData} />
+      <FloatingNav size={0.8} items={pagesData} />
       {!isEmpty(contentBar) && contentBar.map((NavButton, index) => <NavButton key={index} />)}
     </>
   );
 };
 
-const SettingsPages: PagesType[] = [
-  {navButton: {icon: <UserDuo_Icon className="size-full" />, title: PageTitles.dashboard}, path: PageID.dashboard},
-  {navButton: {icon: <Plugins_Icon className="size-full" />, title: PageTitles.plugins}, path: PageID.plugins},
-  {navButton: {icon: <Tuning_Icon className="size-full" />, title: PageTitles.settings}, path: PageID.settings},
+const SettingsPages: NavItem[] = [
+  {icon: <UserDuo_Icon className="size-full" />, title: PageTitles.dashboard, path: PageID.dashboard},
+  {icon: <Plugins_Icon className="size-full" />, title: PageTitles.plugins, path: PageID.plugins},
+  {icon: <Tuning_Icon className="size-full" />, title: PageTitles.settings, path: PageID.settings},
 ];
 
 export function SettingsPagesButtons() {
@@ -134,7 +104,7 @@ export function SettingsPagesButtons() {
 
   return (
     <>
-      <GetPages Pages={SettingsPages} />
+      <FloatingNav size={0.8} items={SettingsPages} />
       {!isEmpty(settingsBar) && settingsBar.map((NavButton, index) => <NavButton key={index} />)}
     </>
   );
