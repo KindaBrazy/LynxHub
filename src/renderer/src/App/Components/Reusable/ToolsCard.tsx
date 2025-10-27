ec7ab76df291e4748d6bd0eddd2379340651ad49
kindabrazy <kindofbrazy@gmail.com>
2025-10-27 12:30:40 +0330

    feat(renderer): add SpotlightCard and ToolsCard components

---
+++ src/renderer/src/App/Components/Reusable/ToolsCard.tsx
@@ -0,0 +1,55 @@
+import {Card, User} from '@heroui/react';
+import {ReactNode} from 'react';
+
+import {useAppState} from '../../Redux/Reducer/AppReducer';
+import SpotlightCard from './SpotlightCard';
+
+type Props = {title: string; description: string; icon: string; onPress: () => void; footer?: ReactNode};
+export function ToolsCard({title, description, icon, onPress, footer}: Props) {
+  const isDarkMode = useAppState('darkMode');
+
+  return (
+    <Card
+      className={
+        `w-[300px] ${footer ? 'h-[210px]' : 'h-[180px]'} relative group transform border border-foreground/5 ` +
+        'transition-all duration-500 hover:-translate-y-[2px] shadow-small hover:shadow-medium ' +
+        'bg-white dark:bg-stone-900 pt-3 pb-2 rounded-3xl hover:border-foreground/15'
+      }
+      onPress={onPress}
+      isPressable>
+      <SpotlightCard className="size-full" spotlightColor={isDarkMode ? '#353535' : '#dadada'}>
+        <div
+          className={
+            'absolute top-0 left-1/2 transform -translate-x-1/2 h-[1px] bg-linear-to-r from-transparent' +
+            ' via-LynxOrange to-transparent rounded-t-full opacity-0 group-hover:opacity-100 transition-all' +
+            ' duration-700 w-0 group-hover:w-[90%]'
+          }
+        />
+
+        {/* Content container */}
+        <div className={`relative h-full flex flex-col ${footer ? 'gap-y-5' : 'gap-y-7'} px-5 pt-4`}>
+          {/* Icon section */}
+          <div className="flex">
+            <User
+              name={title}
+              className="scale-120 ml-4 font-semibold"
+              avatarProps={{src: icon, className: '!bg-transparent', radius: 'none'}}
+            />
+          </div>
+
+          {/* Title and Description */}
+          <p className="text-foreground-500 text-sm line-clamp-3 text-start">{description}</p>
+
+          {footer}
+        </div>
+      </SpotlightCard>
+
+      <div
+        className={
+          'absolute bottom-0 left-1/2 transform -translate-x-1/2 w-20 h-[2px] bg-linear-to-r from-secondary/0' +
+          ' via-secondary to-secondary/0 rounded-t-full group-active:via-foreground transition-colors duration-100'
+        }
+      />
+    </Card>
+  );
+}
