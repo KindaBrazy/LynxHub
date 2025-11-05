0236f2baaba75a94bea04636261ee52b1092af29
kindabrazy <kindofbrazy@gmail.com>
2025-11-05 20:54:13 +0330

    refactor(renderer): memoize lynxTopToast in useLynxToast hook

--- src/renderer/src/App/Utils/UtilHooks.tsx
+++ src/renderer/src/App/Utils/UtilHooks.tsx
@@ -137,7 +137,7 @@ export const lynxTopToast = (dispatch: Dispatch, placement: HeroToastPlacement =
 
 export const useLynxToast = () => {
   const dispatch = useDispatch<AppDispatch>();
-  return lynxTopToast(dispatch);
+  return useMemo(() => lynxTopToast(dispatch), []);
 };
 
 export const useDisableTooltip = (isEssential: boolean = false): boolean => {
