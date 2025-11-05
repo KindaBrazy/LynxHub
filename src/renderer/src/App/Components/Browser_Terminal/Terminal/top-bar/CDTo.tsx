3dd9920faa9b87661212eae0c5f624d225f7c5f5
kindabrazy <kindofbrazy@gmail.com>
2025-11-05 21:03:14 +0330

    feat(renderer): enhance CDTo with dropdown and history support

--- src/renderer/src/App/Components/Browser_Terminal/Terminal/top-bar/CDTo.tsx
+++ src/renderer/src/App/Components/Browser_Terminal/Terminal/top-bar/CDTo.tsx
@@ -1,5 +1,6 @@
-import {Button, Tooltip} from '@heroui/react';
-import {memo, useCallback} from 'react';
+import {Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Tooltip} from '@heroui/react';
+import {isEmpty} from 'lodash';
+import {memo, useCallback, useEffect, useMemo, useState} from 'react';
 
 import {FolderDuo_Icon} from '../../../../../assets/icons/SvgIcons/SvgIcons';
 import rendererIpc from '../../../../RendererIpc';
@@ -7,15 +8,28 @@ import {useLynxToast} from '../../../../Utils/UtilHooks';
 
 type Props = {id: string};
 const CDTo = memo(({id}: Props) => {
+  const [history, setHistory] = useState<string[]>([]);
   const toast = useLynxToast();
 
-  const onButtonPress = useCallback(() => {
+  const cdTo = useCallback(
+    (dir: string) => {
+      if (!history.includes(dir)) {
+        rendererIpc.storage.update('terminal', {cdHistory: [dir, ...history]});
+        setHistory(prevState => [dir, ...prevState]);
+      }
+
+      const LINE_ENDING = window.osPlatform === 'win32' ? '\r' : '\n';
+      rendererIpc.pty.write(id, `cd "${dir}"${LINE_ENDING}`);
+    },
+    [history],
+  );
+
+  const selectDir = useCallback(() => {
     rendererIpc.file
       .openDlg({properties: ['openDirectory']})
       .then(dir => {
         if (dir) {
-          const LINE_ENDING = window.osPlatform === 'win32' ? '\r' : '\n';
-          rendererIpc.pty.write(id, `cd "${dir}"${LINE_ENDING}`);
+          cdTo(dir);
         } else {
           toast.warning('No directory selected');
         }
@@ -24,14 +38,40 @@ const CDTo = memo(({id}: Props) => {
         console.error(e);
         toast.error('Error opening directory');
       });
-  }, [id]);
+  }, [id, cdTo]);
+
+  useEffect(() => {
+    rendererIpc.storage.get('terminal').then(({cdHistory}) => {
+      if (!isEmpty(cdHistory)) setHistory(cdHistory);
+    });
+  }, []);
+
+  const items = useMemo(() => {
+    return [
+      <DropdownItem key="select folder" onPress={selectDir}>
+        Select Folder
+      </DropdownItem>,
+      ...history.map(item => (
+        <DropdownItem key={item} onPress={() => cdTo(item)}>
+          {item}
+        </DropdownItem>
+      )),
+    ];
+  }, [history]);
 
   return (
-    <Tooltip delay={500} content="CD to...">
-      <Button size="sm" variant="light" onPress={onButtonPress} isIconOnly>
-        <FolderDuo_Icon className="size-3.5" />
-      </Button>
-    </Tooltip>
+    <Dropdown>
+      <Tooltip delay={500} content="CD to...">
+        <div className="max-w-fit">
+          <DropdownTrigger>
+            <Button size="sm" variant="light" isIconOnly>
+              <FolderDuo_Icon className="size-3.5" />
+            </Button>
+          </DropdownTrigger>
+        </div>
+      </Tooltip>
+      <DropdownMenu aria-label="cd history">{items}</DropdownMenu>
+    </Dropdown>
   );
 });
 
