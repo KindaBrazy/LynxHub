676f32dd03935295f0ff3d1ccd94936ceaf6a06d
kindabrazy <kindofbrazy@gmail.com>
2025-11-05 20:18:56 +0330

    refactor(renderer): extract CopyAll component from Terminal_TopBar

---
+++ src/renderer/src/App/Components/Browser_Terminal/Terminal/top-bar/CopyAll.tsx
@@ -0,0 +1,35 @@
+import {Button} from '@heroui/react';
+import {SerializeAddon} from '@xterm/addon-serialize';
+import {memo, useCallback, useState} from 'react';
+
+import {CheckDuo_Icon, CopyDuo_Icon} from '../../../../../assets/icons/SvgIcons/SvgIcons';
+
+type Props = {
+  serializeAddon?: SerializeAddon;
+};
+
+const CopyAll = memo(({serializeAddon}: Props) => {
+  const [copied, setCopied] = useState<boolean>(false);
+
+  const handleCopy = useCallback(() => {
+    const contentToCopy = serializeAddon?.serialize();
+    if (!contentToCopy) return;
+
+    navigator.clipboard.writeText(contentToCopy);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  }, [serializeAddon]);
+
+  return (
+    <Button size="sm" variant="light" onPress={handleCopy} isIconOnly>
+      {copied ? (
+        <CheckDuo_Icon className="size-5 animate-appearance-in" />
+      ) : (
+        <CopyDuo_Icon className="size-4 animate-appearance-in" />
+      )}
+    </Button>
+  );
+});
+export default CopyAll;
