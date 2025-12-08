dc2c52cb73ec08023371b0b15ddb69462ab82a05
kindabrazy <kindofbrazy@gmail.com>
2025-12-08 16:13:30 +0330

    refactor(install_stepper): optimize InstallHeader with useMemo and styling

--- src/renderer/src/App/Components/Modals/InstallUI/Install-Header.tsx
+++ src/renderer/src/App/Components/Modals/InstallUI/Install-Header.tsx
@@ -1,6 +1,6 @@
 import {ModalHeader} from '@heroui/react';
 import {Steps} from 'antd';
-import {memo} from 'react';
+import {memo, useMemo} from 'react';
 
 type Props = {
   steps: string[];
@@ -8,21 +8,28 @@ type Props = {
 };
 
 const InstallHeader = ({steps, currentStep}: Props) => {
+  const maxTitleWidth = useMemo(() => (steps.length > 5 ? 'max-w-16' : 'max-w-24'), [steps.length]);
+
   return (
     <ModalHeader className="shrink-0 overflow-hidden bg-foreground-200 shadow-md dark:bg-foreground-100">
       <Steps
         items={steps.map((step, index) => {
           return {
             title: (
-              <span className={`${index < currentStep ? 'text-foreground/40' : 'font-bold text-foreground/80'}`}>
+              <span
+                className={`block truncate text-xs ${maxTitleWidth} ${
+                  index < currentStep ? 'text-foreground/40' : 'font-bold text-foreground/80'
+                }`}
+                title={step}>
                 {step}
               </span>
             ),
           };
         })}
-        type="inline"
+        size="small"
+        type="default"
         current={currentStep}
-        className="!w-full scale-125 items-center justify-center bg-foreground-200 dark:bg-foreground-100"
+        className="!w-full items-center justify-center bg-foreground-200 dark:bg-foreground-100"
       />
     </ModalHeader>
   );
