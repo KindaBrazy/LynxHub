a1c4ded6b983ba76f5e88b64528c1987b53159da
kindabrazy <kindofbrazy@gmail.com>
2025-12-13 11:06:45 +0330

    feat(volume_sync): enhance volume synchronization with audio state updates from browser

--- src/renderer/src/App/AppEvents/VolumeSync.tsx
+++ src/renderer/src/App/AppEvents/VolumeSync.tsx
@@ -1,14 +1,17 @@
 import {useEffect} from 'react';
 import {useDispatch} from 'react-redux';
 
+import {useCardsState} from '../Redux/Reducer/CardsReducer';
 import {volumeActions} from '../Redux/Reducer/VolumeReducer';
 import {AppDispatch} from '../Redux/Store';
 import rendererIpc from '../RendererIpc';
 
-/** Syncs volume state updates from context menu window to Redux */
+/** Syncs volume state updates from context menu window and audio events to Redux */
 export default function useVolumeSync() {
   const dispatch = useDispatch<AppDispatch>();
+  const runningCards = useCardsState('runningCard');
 
+  // Listen for volume/mute updates from context menu
   useEffect(() => {
     const removeVolumeListener = rendererIpc.volume.onTabVolumeUpdate((tabId, volume) => {
       dispatch(volumeActions.setTabVolume({tabId, volume}));
@@ -23,4 +26,16 @@ export default function useVolumeSync() {
       removeMutedListener();
     };
   }, [dispatch]);
+
+  // Listen for audio state changes from browser
+  useEffect(() => {
+    const removeAudioListener = rendererIpc.volume.onAudioStateChange((id, state) => {
+      const card = runningCards.find(c => c.id === id);
+      if (card) {
+        dispatch(volumeActions.setTabAudioPlaying({tabId: card.tabId, playing: state.playing}));
+      }
+    });
+
+    return () => removeAudioListener();
+  }, [dispatch, runningCards]);
 }
