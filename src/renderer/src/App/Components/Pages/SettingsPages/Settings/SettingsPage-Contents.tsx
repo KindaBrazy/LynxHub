fe1644964ef15f0b1a753c042f44fdcef3ab48f2
kindabrazy <kindofbrazy@gmail.com>
2026-01-14 14:01:19 +0330

    feat(settings): show just selected section

--- src/renderer/src/App/Components/Pages/SettingsPages/Settings/SettingsPage-Contents.tsx
+++ src/renderer/src/App/Components/Pages/SettingsPages/Settings/SettingsPage-Contents.tsx
@@ -1,19 +1,14 @@
 import LynxScroll from '../../../Reusable/LynxScroll';
-import SettingsContentSkeleton from '../SettingsContentSkeleton';
-import {useDelayedShow} from '../useDelayedShow';
 import {SettingsSections} from './SettingsContainer';
 
 type Props = {sectionTexts: Map<string, string>};
 
 /** Settings content */
 const SettingsPageContents = ({sectionTexts}: Props) => {
-  const showSections = useDelayedShow(500);
-
   return (
     <LynxScroll className="size-full pl-1 pr-4 py-2">
       <div className="flex flex-col gap-y-4">
-        {!showSections && <SettingsContentSkeleton />}
-        {showSections && <SettingsSections sectionTexts={sectionTexts} />}
+        <SettingsSections sectionTexts={sectionTexts} />
       </div>
     </LynxScroll>
   );
