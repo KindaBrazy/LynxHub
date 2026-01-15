532e5da586b9e05619d421eb98ab0017bcf4a8cb
kindabrazy <kindofbrazy@gmail.com>
2026-01-15 13:16:02 +0330

    feat(renderer): added `HeroProvider` and `useDocumentDarkMode`

---
+++ src/renderer/CrossHooks.tsx
@@ -0,0 +1,23 @@
+import {useCallback, useEffect, useState} from 'react';
+
+import rendererIpc from './src/App/RendererIpc';
+
+export function useDocumentDarkMode(className?: string) {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
+
+  const setDark = useCallback((isDark: boolean) => {
+    document.documentElement.className = `${isDark ? 'dark' : 'light'} ${className}`;
+    setIsDarkMode(true);
+  }, []);
+
+  useEffect(() => {
+    rendererIpc.win.isDarkMode().then(setDark);
+    const offDarkMode = rendererIpc.win.onDarkMode((_, isDark) => {
+      setDark(isDark);
+    });
+
+    return () => offDarkMode();
+  }, []);
+
+  return isDarkMode;
+}
