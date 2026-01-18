61bb4707123f4b0b456ed95d36c9eaf42e52b91c
kindabrazy <kindofbrazy@gmail.com>
2026-01-18 09:30:34 +0330

    refactor(downloads): remove unused files and entries

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -99,7 +99,6 @@ export default defineConfig(({mode}) => {
             prompt_dialog: resolve(__dirname, 'src/renderer/prompt_dialog.html'),
             context_menu: resolve(__dirname, 'src/renderer/context_menu.html'),
             toast: resolve(__dirname, 'src/renderer/toast.html'),
-            downloads_menu: resolve(__dirname, 'src/renderer/downloads_menu.html'),
             share_screen: resolve(__dirname, 'src/renderer/share_screen.html'),
             link_preview: resolve(__dirname, 'src/renderer/link_preview.html'),
           },
