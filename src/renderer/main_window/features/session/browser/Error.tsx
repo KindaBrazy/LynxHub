277b61b8c862a7170bab1d2930dec0b8e44e44bd
kindabrazy <kindofbrazy@gmail.com>
2026-01-27 13:28:05 +0330

    refactor(error): replace icons with Solar icons, enhance button styles, and improve error details layout

--- src/renderer/main_window/features/session/browser/Error.tsx
+++ src/renderer/main_window/features/session/browser/Error.tsx
@@ -1,8 +1,7 @@
-import {Accordion, AccordionItem, Button, Card} from '@heroui/react';
+import {Accordion, AccordionItem, Button} from '@heroui/react';
 import CopyClipboard from '@lynx/components/CopyClipboard';
-import {Earth} from '@solar-icons/react-perf/BoldDuotone';
+import {LinkBroken, Restart} from '@solar-icons/react-perf/BoldDuotone';
 import {motion} from 'framer-motion';
-
 type FailedLoad = {errorCode: number; errorDescription: string; validatedURL: string};
 
 type Props = {
@@ -18,17 +17,19 @@ export function Browser_Error({error, onReload}: Props) {
       <motion.div
         initial={{opacity: 0, translateY: 5}}
         animate={{opacity: 1, translateY: 0, transition: {delay: 0.2}}}
-        className="flex w-full max-w-[50%] flex-col items-center gap-6 text-center">
-        <Earth className="size-20 text-warning" />
+        className="flex w-full max-w-xl flex-col items-center gap-6 text-center">
+        <LinkBroken className="size-20 text-warning" />
         <div className="flex w-full flex-col gap-2">
-          <h1 className="text-2xl font-bold text-foreground-800">This page isn't available</h1>
+          <div>
+            <h1 className="text-2xl font-bold text-warning">This page isn't available</h1>
+          </div>
           <p title={error.validatedURL} className="w-full line-clamp-2 text-sm text-foreground-500">
             Could not load <span className="font-medium">{error.validatedURL}</span>
           </p>
         </div>
 
         <div className="flex items-center gap-4">
-          <Button color="primary" className="w-36" onPress={onReload}>
+          <Button color="primary" className="w-36" onPress={onReload} startContent={<Restart />}>
             Reload
           </Button>
         </div>
@@ -42,20 +43,37 @@ export function Browser_Error({error, onReload}: Props) {
           </ul>
         </div>
 
-        <Accordion>
+        <Accordion variant="light">
           <AccordionItem
-            classNames={{
-              title: 'text-foreground-500 text-sm',
-            }}
-            title="Show details">
-            <Card className="gap-y-2 pt-3 text-start">
-              <pre className="whitespace-pre-wrap break-all px-3 font-JetBrainsMono! text-xs text-foreground-600">
-                <code>
-                  {`URL: ${error.validatedURL}\nError Code: ${error.errorCode}\nDescription: ${error.errorDescription}`}
-                </code>
-              </pre>
-              <CopyClipboard contentToCopy={details} className="w-full bg-foreground-200 cursor-pointer" />
-            </Card>
+            title={
+              <div className="flex items-center gap-x-2">
+                Show details
+                <CopyClipboard contentToCopy={details} />
+              </div>
+            }
+            className="max-w-lg">
+            <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-1 rounded-lg bg-foreground-100 p-3">
+                  <span className="text-xs font-semibold text-foreground-500">URL</span>
+                  <code className="break-all font-JetBrainsMono! text-xs text-foreground-800">
+                    {error.validatedURL}
+                  </code>
+                </div>
+
+                <div className="flex flex-col gap-1 rounded-lg bg-foreground-100 p-3 ">
+                  <span className="text-xs font-semibold text-danger">Error Code</span>
+                  <code className="font-JetBrainsMono! text-xs text-danger-700">{error.errorCode}</code>
+                </div>
+
+                <div className="flex flex-col gap-1 rounded-lg bg-foreground-100 p-3 ">
+                  <span className="text-xs font-semibold text-warning">Description</span>
+                  <code className="break-all font-JetBrainsMono! text-xs text-warning-700">
+                    {error.errorDescription}
+                  </code>
+                </div>
+              </div>
+            </div>
           </AccordionItem>
         </Accordion>
       </motion.div>
