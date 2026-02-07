db3f43fba004babf153a0999c17101ef5ca23450
KindaBrazy <kindofbrazy@gmail.com>
2026-02-07 12:19:01 +0330

    style(eslint): disable no-useless-escape rule

--- eslint.config.js
+++ eslint.config.js
@@ -85,6 +85,7 @@ export default defineConfig([
       'max-len': ['error', {code: MAX_LINE_LENGTH}],
 
       'sonarjs/os-command': 'off',
+      'no-useless-escape': 'off',
       'prettier/prettier': [
         'error',
         {
