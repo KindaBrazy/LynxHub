b3885f22d4f0510c1adfffec06afd600bc24f613
KindaBrazy <kindofbrazy@gmail.com>
2026-02-09 12:38:22 +0330

    fix(tools_page): fixed page not loading modules

--- src/renderer/main_window/pages/tools/index.tsx
+++ src/renderer/main_window/pages/tools/index.tsx
@@ -1,20 +1,12 @@
 import {ScrollShadow} from '@heroui/react';
+import {GetComponentsByPath} from '@lynx/components/card';
 import {extensionsData} from '@lynx/plugins/extensions/loader';
 import {ToolsPage_Icon} from '@lynx_assets/icons/pages';
-import {motion, Variants} from 'framer-motion';
+import {PageID} from '@lynx_common/consts';
 import {memo} from 'react';
 
 import CardsContainer, {CardContainerClasses} from '../CardsContainer';
 import Page from '../Page';
-
-const variants: Variants = {
-  initial: {opacity: 0, translateY: 20},
-  animate: (index: number) => ({
-    opacity: 1,
-    translateY: 0,
-    transition: {delay: index * 0.05},
-  }),
-};
 
 type Props = {show: boolean};
 
@@ -29,19 +21,7 @@ const ToolsPage = memo(({show}: Props) => {
           extraClassNames="mr-3"
           subTitle="Essential AI Utilities for Your Daily Tasks"
           icon={<ToolsPage_Icon className={CardContainerClasses} />}>
-          <div className="flex size-full flex-row flex-wrap gap-7 overflow-visible">
-            {addComponent.map((Comp, index) => (
-              <motion.div
-                custom={index}
-                initial="initial"
-                animate="animate"
-                variants={variants}
-                key={`${index}_tools_card`}
-                layout>
-                <Comp key={index} />{' '}
-              </motion.div>
-            ))}
-          </div>
+          <GetComponentsByPath routePath={PageID.tools} extensionsElements={addComponent} />
         </CardsContainer>
       </ScrollShadow>
     </Page>
