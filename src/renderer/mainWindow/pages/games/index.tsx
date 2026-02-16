61135350c351dc56fd6e82986677696aa60cfe78
KindaBrazy <kindofbrazy@gmail.com>
2026-02-16 14:41:37 +0330

    refactor(extensions): expand page customization with positional component slots

--- src/renderer/mainWindow/pages/games/index.tsx
+++ src/renderer/mainWindow/pages/games/index.tsx
@@ -3,7 +3,7 @@ import {GetComponentsByPath} from '@lynx/components/card';
 import {extensionsData} from '@lynx/plugins/extensions/loader';
 import {GamePage_Icon} from '@lynx_assets/icons/pages';
 import {PageID} from '@lynx_common/consts';
-import {memo} from 'react';
+import {memo, useMemo} from 'react';
 
 import CardsContainer, {CardContainerClasses} from '../CardsContainer';
 import Page from '../Page';
@@ -11,19 +11,32 @@ import Page from '../Page';
 type Props = {show: boolean};
 
 const GamesPage = memo(({show}: Props) => {
-  const {addComponent} = extensionsData.customizePages.games;
+  const {top, scrollTop, scrollBottom, bottom, cardsContainer} = useMemo(
+    () => extensionsData.customizePages.games.add,
+    [],
+  );
+
+  console.log('ccc', cardsContainer);
 
   return (
     <Page show={show}>
+      {top && top.map((Top, index) => <Top key={index} />)}
+
       <ScrollShadow size={20} className="size-full overflow-y-scroll p-5 scrollbar-hide">
+        {scrollTop && scrollTop.map((ScrollTop, index) => <ScrollTop key={index} />)}
+
         <CardsContainer
           title="Games"
           extraClassNames="mr-3"
           subTitle="Enjoy Games, Hobbies, and Fun AI Tools"
           icon={<GamePage_Icon className={CardContainerClasses} />}>
-          <GetComponentsByPath routePath={PageID.games} extensionsElements={addComponent} />
+          <GetComponentsByPath routePath={PageID.games} extensionsElements={cardsContainer} />
         </CardsContainer>
+
+        {scrollBottom && scrollBottom.map((ScrollBottom, index) => <ScrollBottom key={index} />)}
       </ScrollShadow>
+
+      {bottom && bottom.map((Bottom, index) => <Bottom key={index} />)}
     </Page>
   );
 });
