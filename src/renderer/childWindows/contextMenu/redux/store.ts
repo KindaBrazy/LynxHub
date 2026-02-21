889d08ea3c4f57ad28f8ab891c1d7fa653d341da
KindaBrazy <kindofbrazy@gmail.com>
2026-02-21 13:33:07 +0330

    refactor(renderer): done childWindows/contextMenu folder

--- src/renderer/childWindows/contextMenu/redux/store.ts
+++ src/renderer/childWindows/contextMenu/redux/store.ts
@@ -3,5 +3,13 @@ import {configureStore} from '@reduxjs/toolkit';
 import context from './reducer';
 
 export const store = configureStore({reducer: {context}});
+
+/**
+ * The root state type of the context menu store.
+ */
 export type RootState = ReturnType<typeof store.getState>;
+
+/**
+ * The dispatch type of the context menu store.
+ */
 export type ContextDispatch = typeof store.dispatch;
--- src/renderer/childWindows/contextMenu/types.ts
+++ src/renderer/childWindows/contextMenu/types.ts
@@ -1,3 +1,45 @@
+import {DownloadItemInfo} from '@lynx_common/types/downloadManager';
+import {ContextMenuVolumeData, NavHistory} from '@lynx_common/types/ipc';
+import {ContextMenuParams} from 'electron';
 import {ReactNode} from 'react';
+
+import {MenuTypes} from './consts';
+
+export type ZoomLayout = {id: string; factor: number};
+
+export type RightClick = {
+  id: number;
+  contextMenuParams: ContextMenuParams | undefined;
+  navigationHistory: NavHistory;
+};
+
+export type RightClickParams = {
+  hasLinkItems: boolean;
+  hasImageItems: boolean;
+  hasTextSelection: boolean;
+  hasEditItems: boolean;
+  isActionsAvailable: boolean;
+};
+
+export type PromptWindow = {message: string; defaultValue?: string};
+export type AlertWindow = {message: string};
+export type ConfirmWindow = {message: string};
+
+export type ContextState = {
+  windowWidth: string;
+  activeLayout: MenuTypes | undefined;
+  targetID: string;
+  selectedText: string;
+
+  browserScale: ZoomLayout;
+  browserVolume: ContextMenuVolumeData;
+  promptWindow: PromptWindow;
+
+  rightClick: RightClick;
+  rightClickParams: RightClickParams;
+  alertWindow: AlertWindow;
+  confirmWindow: ConfirmWindow;
+  downloads: DownloadItemInfo[];
+};
 
 export type NavBtnProps = {icon?: ReactNode; onPress?: () => void; className?: string; isDisabled?: boolean};
