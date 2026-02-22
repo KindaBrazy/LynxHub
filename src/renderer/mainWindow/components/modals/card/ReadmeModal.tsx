1ba4f8fb70906670bb579adeaf4f4b1083ad2136
KindaBrazy <kindofbrazy@gmail.com>
2026-02-22 11:34:28 +0330

    refactor(renderer): done `modals/card` folder

--- src/renderer/mainWindow/components/modals/card/ReadmeModal.tsx
+++ src/renderer/mainWindow/components/modals/card/ReadmeModal.tsx
@@ -1,16 +1,21 @@
 import {Button, Link, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, User} from '@heroui/react';
 import {extractGitUrl, getCacheUrl} from '@lynx_common/utils';
 import {isEmpty, isNil} from 'lodash';
-import {Fragment, useMemo} from 'react';
+import {Fragment, memo, useMemo} from 'react';
 
 import {extensionsData} from '../../../plugins/extensions/loader';
 import {useModalsState} from '../../../redux/reducers/modals';
 import MarkdownViewer from '../../MarkdownViewer';
 import {useTabModalLifecycle} from '../useTabModalManager';
 
-type Props = {isOpen: boolean; url: string; title: string; tabID: string};
+type Props = {
+  isOpen: boolean;
+  url: string;
+  title: string;
+  tabID: string;
+};
 
-const CardReadmeModal = ({isOpen, url, title, tabID}: Props) => {
+const CardReadmeDialog = memo(({isOpen, url, title, tabID}: Props) => {
   const {onOpenChange, show} = useTabModalLifecycle('readme', tabID);
 
   const ReplaceMd = useMemo(() => extensionsData.replaceMarkdownViewer, []);
@@ -25,11 +30,14 @@ const CardReadmeModal = ({isOpen, url, title, tabID}: Props) => {
       scrollBehavior="inside"
       className="max-w-[95%]"
       onOpenChange={onOpenChange}
-      classNames={{backdrop: `top-10! ${show}`, closeButton: 'cursor-default', wrapper: `top-10! ${show}`}}
+      classNames={{
+        backdrop: `top-10! ${show}`,
+        closeButton: 'cursor-default',
+        wrapper: `top-10! ${show}`,
+      }}
       hideCloseButton>
       <ModalContent className="overflow-hidden">
-        <ModalHeader
-          className={'shrink-0 justify-center overflow-hidden bg-foreground-200 shadow-md dark:bg-foreground-100'}>
+        <ModalHeader className="shrink-0 justify-center overflow-hidden bg-foreground-200 shadow-md dark:bg-foreground-100">
           <User
             description={
               <Link size="sm" href={url} isExternal>
@@ -49,32 +57,28 @@ const CardReadmeModal = ({isOpen, url, title, tabID}: Props) => {
             ))}
         </ModalBody>
         <ModalFooter>
-          <Button
-            onPress={() => {
-              onOpenChange(false);
-            }}
-            variant="light"
-            color="warning">
+          <Button onPress={() => onOpenChange(false)} variant="light" color="warning">
             Close
           </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
   );
-};
+});
 
-const ReadMeModal = () => {
+const ReadmeModal = () => {
   const CardReadme = useMemo(() => extensionsData.replaceModals.cardReadme, []);
-
   const readmeModals = useModalsState('readmeModal');
 
   return (
     <>
       {readmeModals.map(modal => (
-        <Fragment key={`${modal.tabID}_modal`}>{CardReadme ? <CardReadme /> : <CardReadmeModal {...modal} />}</Fragment>
+        <Fragment key={`${modal.tabID}_modal`}>
+          {CardReadme ? <CardReadme /> : <CardReadmeDialog {...modal} />}
+        </Fragment>
       ))}
     </>
   );
 };
 
-export default ReadMeModal;
+export default ReadmeModal;
---
+++ src/renderer/mainWindow/components/modals/card/UpdatingModal/hooks/useUpdatingProgress.tsx
@@ -0,0 +1,114 @@
+import {Button} from '@heroui/react';
+import {GitProgressCallback} from '@lynx_common/types/ipc';
+import gitIpc from '@lynx_shared/ipc/git';
+import {List} from '@solar-icons/react-perf/BoldDuotone';
+import {Descriptions, notification} from 'antd';
+import {isEmpty} from 'lodash';
+import {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import {PullResult} from 'simple-git';
+
+import {cardsActions, useCardsState} from '../../../../../redux/reducers/cards';
+import {modalActions} from '../../../../../redux/reducers/modals';
+import {useTabsState} from '../../../../../redux/reducers/tabs';
+import {AppDispatch} from '../../../../../redux/store';
+
+/**
+ * Hook to listen for git update progress and show notifications.
+ */
+export function useUpdatingProgress() {
+  const updatingCards = useCardsState('updatingCards');
+  const dispatch = useDispatch<AppDispatch>();
+  const activeTab = useTabsState('activeTab');
+
+  useEffect(() => {
+    if (isEmpty(updatingCards)) return;
+
+    const onProgress: GitProgressCallback = (id, state, result) => {
+      if (!id) return;
+
+      const card = updatingCards.find(value => value.id === id);
+      if (!card) return;
+
+      notification.destroy(`${card.devName}-update-error`);
+
+      if (state === 'Failed') {
+        notification.error({
+          key: `${card.devName}-update-error`,
+          description: <div className="whitespace-pre-line">{typeof result === 'string' && result}</div>,
+          title: (
+            <div className="whitespace-pre-line">
+              Failed to Update {card.title} ({card.devName})
+            </div>
+          ),
+        });
+
+        dispatch(cardsActions.removeUpdatingCard(card.id));
+      } else if (state === 'Completed' && result) {
+        const {
+          summary: {changes, insertions, deletions},
+        } = result as PullResult;
+        notification.success({
+          closeIcon: null,
+          actions: (
+            <div className="flex flex-row gap-x-2">
+              <Button
+                onPress={() => {
+                  if ('summary' in result) {
+                    dispatch(
+                      modalActions.openUpdateDetails({
+                        details: result,
+                        title: `${card.title} (${card.devName}) Update Details.`,
+                        tabID: activeTab,
+                      }),
+                    );
+                  }
+                  notification.destroy(`${card.devName}-updateDetails`);
+                }}
+                size="sm"
+                variant="flat"
+                color="success"
+                startContent={<List />}>
+                Details
+              </Button>
+              <Button
+                onPress={() => {
+                  notification.destroy(`${card.devName}-updateDetails`);
+                }}
+                size="sm"
+                color="warning"
+                variant="light">
+                Close
+              </Button>
+            </div>
+          ),
+          description: (
+            <Descriptions
+              items={[
+                {children: insertions, label: 'Insertions'},
+                {children: deletions, label: 'Deletions'},
+                {children: changes, label: 'Changes'},
+              ]}
+              column={2}
+              size="small"
+            />
+          ),
+          duration: 0,
+          key: `${card.devName}-updateDetails`,
+          className: 'top-10! dark:bg-foreground-100 !shadow-medium !overflow-hidden rounded-xl',
+          title: (
+            <span className="font-semibold">
+              {card.title} ({card.devName}) Updated Successfully
+            </span>
+          ),
+        });
+        dispatch(cardsActions.removeUpdatingCard(card.id));
+        dispatch(cardsActions.removeUpdateAvailable(card.id));
+      }
+    };
+
+    const removeListener = gitIpc.onProgress(onProgress);
+
+    return () => removeListener();
+  }, [updatingCards, dispatch, activeTab]);
+}
