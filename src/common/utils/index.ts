04b359335ccbde554bf33f683ad0979687d4c234
KindaBrazy <kindofbrazy@gmail.com>
2026-03-05 15:15:33 +0330

    fix(terminal): remove multiple ending lines from commands

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -3,10 +3,12 @@
  * Exports all utility functions from sub-modules.
  */
 
+export * from './arrays';
 export * from './env';
 export * from './fileUtils';
 export * from './formatting';
 export * from './platform';
 export * from './plugins';
+export * from './strings';
 export * from './time';
 export * from './urlUtils';
