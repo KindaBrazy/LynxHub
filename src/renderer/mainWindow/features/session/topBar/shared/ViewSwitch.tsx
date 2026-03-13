2f768145be803b3212652ba2fc43f7390e96cc08
KindaBrazy <kindofbrazy@gmail.com>
2026-03-13 09:58:28 +0330

    style(topbar): update switch view width

--- src/renderer/mainWindow/features/session/topBar/shared/ViewSwitch.tsx
+++ src/renderer/mainWindow/features/session/topBar/shared/ViewSwitch.tsx
@@ -30,7 +30,7 @@ const ViewSwitch = memo(({currentView}: Props) => {
 
   return (
     <Tooltip delay={500} content={tooltipContent}>
-      <Button size="sm" variant="light" color="primary" onPress={handlePress} aria-label={tooltipContent} isIconOnly>
+      <Button size="sm" variant="light" color="primary" onPress={handlePress} aria-label={tooltipContent}>
         {isBrowser ? <Terminal_Icon className="size-4" /> : <Earth className="size-4" />}
       </Button>
     </Tooltip>
