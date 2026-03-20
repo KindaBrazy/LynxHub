d2bef54283859b3d1884d8961f10073e2ac8f93a
KindaBrazy <kindofbrazy@gmail.com>
2026-03-20 12:01:49 +0330

    feat(arguments): add basic environment and command line items

--- src/renderer/mainWindow/components/modals/card/LaunchConfigModal/Arguments/CustomArguments.tsx
+++ src/renderer/mainWindow/components/modals/card/LaunchConfigModal/Arguments/CustomArguments.tsx
@@ -1,6 +1,26 @@
-import {Button, Divider, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input} from '@heroui/react';
+import {
+  Button,
+  Divider,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Input,
+  NumberInput,
+  Select,
+  SelectItem,
+  SharedSelection,
+} from '@heroui/react';
 import {ArgumentType} from '@lynx_common/types/plugins/modules';
-import {HomeAddAngle, Inbox, Pen2, SettingsMinimalistic, TrashBin2} from '@solar-icons/react-perf/BoldDuotone';
+import filesIpc from '@lynx_shared/ipc/files';
+import {
+  CloseCircle,
+  HomeAddAngle,
+  Inbox,
+  Pen2,
+  SettingsMinimalistic,
+  TrashBin2,
+} from '@solar-icons/react-perf/BoldDuotone';
 import {CheckRead, Unread} from '@solar-icons/react-perf/Linear';
 import {AnimatePresence, motion} from 'framer-motion';
 import {some} from 'lodash';
@@ -9,8 +29,8 @@ import {Dispatch, ReactNode, SetStateAction, useState} from 'react';
 
 import EmptyStateCard from '../../../../EmptyStateCard';
 
-type ItemTypes = 'envVar' | 'commandLine' | 'comment';
-type CustomItem = {name: string; type: ItemTypes; kind: ArgumentType; defaultValue?: any};
+type ItemKinds = 'envVar' | 'commandLine' | 'comment';
+type CustomItem = {name: string; kind: ItemKinds; type: ArgumentType; defaultValue?: any};
 
 type AnimProp = {
   children: ReactNode;
@@ -28,7 +48,7 @@ function generateUniqueName(items: CustomItem[], baseName: string): string {
   let newName = baseName;
 
   while (!isNameUnique(items, newName)) {
-    newName = `${baseName} (${counter++})`;
+    newName = `${baseName}_${counter++}`;
   }
 
   return newName;
@@ -64,7 +84,7 @@ function RenderItem(
     );
   };
 
-  const onDefaultValueChange = (value: string) => {
+  const onDefaultValueChange = (value: any) => {
     setCustomList(prevState =>
       prevState.map(listItem => {
         return listItem.name !== item.name ? listItem : {...listItem, defaultValue: value};
@@ -72,23 +92,45 @@ function RenderItem(
     );
   };
 
-  switch (item.type) {
+  const onTypeChange = (keys: SharedSelection) => {
+    const targetKey = Array.from(keys)[0] as ArgumentType;
+    setCustomList(prevState =>
+      prevState.map(listItem => (listItem.name === item.name ? {...listItem, type: targetKey} : listItem)),
+    );
+  };
+
+  const selectFolder = () => {
+    filesIpc.openDlg({properties: ['openDirectory']}).then(selectedPath => {
+      if (selectedPath) onDefaultValueChange(selectedPath);
+    });
+  };
+
+  const selectFile = () => {
+    filesIpc.openDlg({properties: ['openFile']}).then(selectedPath => {
+      if (selectedPath) onDefaultValueChange(selectedPath);
+    });
+  };
+
+  const clearDefaultValue = () => onDefaultValueChange(undefined);
+
+  switch (item.kind) {
     case 'envVar':
-      return <span key={item.name}>{item.name}</span>;
     case 'commandLine':
-      return <span key={item.name}>{item.name}</span>;
-    case 'comment':
       return (
-        <AnimatePresence key={item.name} mode="popLayout">
+        <AnimatePresence>
           <motion.div
             animate={{translateY: 0, opacity: 1, scale: 1}}
             initial={{translateY: 5, opacity: 0, scale: 0.9}}
-            className="text-sm bg-background flex flex-col p-4 rounded-xl gap-y-2"
+            className="text-sm bg-background dark:bg-LynxNearBlack flex flex-col px-4 py-3 rounded-xl gap-y-2"
             layout>
             <div className="flex justify-between items-center">
-              <div className="flex items-center gap-x-2 text-warning font-semibold">
-                <Pen2 />
-                <span>Comment</span>
+              <div
+                className={
+                  `flex items-center gap-x-2 font-semibold ` +
+                  `${item.kind === 'envVar' ? 'text-primary-500' : 'text-secondary-500'}`
+                }>
+                {item.kind === 'envVar' ? <HomeAddAngle /> : <SettingsMinimalistic />}
+                <span>{item.kind === 'envVar' ? 'Environment Variable' : 'Command Line'}</span>
               </div>
               <div className="flex items-center gap-x-1">
                 <AnimateChild show={isAdded}>
@@ -117,7 +159,141 @@ function RenderItem(
               <Divider className="h-4" orientation="vertical" />
 
               <div className="flex items-center gap-x-2 flex-1">
-                <span className="shrink-0">Default Value:</span>
+                <span className="shrink-0">Type:</span>
+                <Select
+                  size="sm"
+                  selectedKeys={[item.type]}
+                  onSelectionChange={onTypeChange}
+                  aria-label="Select the argument type">
+                  <SelectItem key="Input" variant="flat">
+                    Input (Text)
+                  </SelectItem>
+                  <SelectItem key="Number" variant="flat">
+                    Number
+                  </SelectItem>
+                  <SelectItem variant="flat" key="Directory">
+                    Folder
+                  </SelectItem>
+                  <SelectItem key="File" variant="flat">
+                    File
+                  </SelectItem>
+                  <SelectItem variant="flat" key="CheckBox">
+                    Boolean
+                  </SelectItem>
+                  <SelectItem variant="flat" key="DropDown">
+                    Select (Predefined Values)
+                  </SelectItem>
+                </Select>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-x-2">
+              {item.type === 'Input' && (
+                <>
+                  <span className="shrink-0">Default Value:</span>
+                  <Input
+                    size="sm"
+                    spellCheck="false"
+                    onValueChange={onDefaultValueChange}
+                    placeholder="Enter defualt value..."
+                    value={typeof item.defaultValue !== 'string' ? '' : item.defaultValue}
+                    fullWidth
+                    isClearable
+                  />
+                </>
+              )}
+              {item.type === 'Number' && (
+                <>
+                  <span className="shrink-0">Default Value:</span>
+                  <NumberInput
+                    size="sm"
+                    spellCheck="false"
+                    onValueChange={onDefaultValueChange}
+                    placeholder="Enter defualt value..."
+                    value={typeof item.defaultValue === 'string' ? 0 : item.defaultValue}
+                    fullWidth
+                    isClearable
+                  />
+                </>
+              )}
+              {item.type === 'Directory' && (
+                <>
+                  <span className="shrink-0">Folder:</span>
+                  <Button
+                    endContent={
+                      item.defaultValue && (
+                        <Button size="sm" variant="light" className="size-6" onPress={clearDefaultValue} isIconOnly>
+                          <CloseCircle className="size-4" />
+                        </Button>
+                      )
+                    }
+                    size="sm"
+                    variant="flat"
+                    onPress={selectFolder}
+                    className={`justify-between ${item.defaultValue && 'text-foreground'}`}
+                    fullWidth>
+                    {item.defaultValue || 'Press to set default folder...'}
+                  </Button>
+                </>
+              )}
+              {item.type === 'File' && (
+                <>
+                  <span className="shrink-0">Folder:</span>
+                  <Button
+                    endContent={
+                      item.defaultValue && (
+                        <Button size="sm" variant="light" className="size-6" onPress={clearDefaultValue} isIconOnly>
+                          <CloseCircle className="size-4" />
+                        </Button>
+                      )
+                    }
+                    size="sm"
+                    variant="flat"
+                    onPress={selectFile}
+                    className={`justify-between ${item.defaultValue && 'text-foreground'}`}
+                    fullWidth>
+                    {item.defaultValue || 'Press to set default file...'}
+                  </Button>
+                </>
+              )}
+            </div>
+          </motion.div>
+        </AnimatePresence>
+      );
+    case 'comment':
+      return (
+        <AnimatePresence>
+          <motion.div
+            animate={{translateY: 0, opacity: 1, scale: 1}}
+            initial={{translateY: 5, opacity: 0, scale: 0.9}}
+            className="text-sm bg-background dark:bg-LynxNearBlack flex flex-col px-4 py-3 rounded-xl gap-y-2"
+            layout>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-x-2 text-warning-400 font-semibold">
+                <Pen2 />
+                <span>Comment</span>
+              </div>
+              <div className="flex items-center gap-x-1">
+                <AnimateChild show={isAdded}>
+                  <Button size="sm" color="success" variant="light" isIconOnly>
+                    <CheckRead className="size-4.5" />
+                  </Button>
+                </AnimateChild>
+                <AnimateChild show={!isAdded}>
+                  <Button size="sm" variant="light" onPress={() => addItem(item)} isIconOnly>
+                    <Unread className="size-4.5" />
+                  </Button>
+                </AnimateChild>
+
+                <Button size="sm" color="danger" variant="light" onPress={() => removeItem(item)} isIconOnly>
+                  <TrashBin2 className="size-3.5" />
+                </Button>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-x-4">
+              <div className="flex items-center gap-x-2 flex-1">
+                <span className="shrink-0">Comment Text:</span>
                 <Input
                   size="sm"
                   spellCheck="false"
@@ -138,11 +314,11 @@ export default function CustomArguments() {
   const [customList, setCustomList] = useState<CustomItem[]>([]);
   const [addedList, setAddedList] = useState<CustomItem[]>([]);
 
-  const addCustom = (itemType: ItemTypes) => {
+  const addCustom = (itemKind: ItemKinds) => {
     setCustomList(prevState => {
       let baseName: string;
 
-      switch (itemType) {
+      switch (itemKind) {
         case 'envVar':
           baseName = 'environment';
           break;
@@ -160,8 +336,8 @@ export default function CustomArguments() {
       const uniqueName = generateUniqueName(prevState, baseName);
       const itemToAdd: CustomItem = {
         name: uniqueName,
-        kind: 'Input',
-        type: itemType,
+        type: 'Input',
+        kind: itemKind,
       };
 
       return [...prevState, itemToAdd];
@@ -182,7 +358,7 @@ export default function CustomArguments() {
   };
 
   return (
-    <div className="bg-foreground-100 rounded-xl flex flex-col size-full items-center p-4">
+    <div className="bg-foreground-100 rounded-xl flex flex-col inset-0 items-center p-4 relative">
       <div className="size-full flex justify-between mb-4">
         <div />
         <span className="font-bold text-LynxOrange">Custom Arguments List</span>
