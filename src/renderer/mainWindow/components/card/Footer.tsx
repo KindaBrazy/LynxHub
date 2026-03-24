69d04219945bdb2e342605ad1eb8f0978915cfeb
KindaBrazy <kindofbrazy@gmail.com>
2026-03-24 15:45:01 +0330

    refactor(card): move open folder button to footer and make it wide and flat

--- src/renderer/mainWindow/components/card/Footer.tsx
+++ src/renderer/mainWindow/components/card/Footer.tsx
@@ -2,14 +2,17 @@ import {Button, CardFooter} from '@heroui/react';
 import ShinyText from '@lynx/components/ShinyText';
 import {extensionsData} from '@lynx/plugins/extensions/loader';
 import {useAppState} from '@lynx/redux/reducers/app';
-import {useIsPinnedCard} from '@lynx/utils/hooks';
+import {useInstalledCard, useIsPinnedCard} from '@lynx/utils/hooks';
+import filesIpc from '@lynx_shared/ipc/files';
 import {storageUtilsIpc} from '@lynx_shared/ipc/storage';
 import AddBreadcrumb_Renderer from '@lynx_shared/sentry/Breadcrumbs';
 import {Pin, Play} from '@solar-icons/react-perf/Bold';
-import {DownloadMinimalistic} from '@solar-icons/react-perf/BoldDuotone';
+import {DownloadMinimalistic, FolderOpen} from '@solar-icons/react-perf/BoldDuotone';
 import {Pin as PinLine} from '@solar-icons/react-perf/LineDuotone';
+import {AnimatePresence, motion} from 'framer-motion';
 import {memo, useCallback, useMemo} from 'react';
 
+import {useHotkeysState} from '../../redux/reducers/hotkeys';
 import InstalledMenu from './menu/Installed';
 import UninstalledMenu from './menu/Uninstalled';
 import {useCardStore} from './store';
@@ -32,10 +35,21 @@ const Footer = memo(({isRunning, updatingExtensions, updating, updateCount, id}:
   const isInstalled = useCardStore(state => state.installed);
   const isPinned = useIsPinnedCard(id);
 
+  const isCtrlPressed = useHotkeysState('isCtrlPressed');
+  const webUI = useInstalledCard(id);
+
   const onPress = useCallback(() => {
     AddBreadcrumb_Renderer(`Pin AI: id:${id} , ${isPinned ? 'remove' : 'add'}`);
     storageUtilsIpc.invoke.pinnedCards(isPinned ? 'remove' : 'add', id);
   }, [isPinned, id]);
+
+  const showOpenFolder = useMemo(() => {
+    return !!webUI?.dir && isCtrlPressed;
+  }, [webUI, isCtrlPressed]);
+
+  const openFolder = () => {
+    if (webUI && webUI.dir) filesIpc.openPath(webUI.dir);
+  };
 
   const ReplaceMenu = useMemo(() => extensionsData.cards.customize.menu.replace, []);
 
@@ -86,6 +100,25 @@ const Footer = memo(({isRunning, updatingExtensions, updating, updateCount, id}:
           {isInstalled ? <Play className="size-4 text-primary" /> : <DownloadMinimalistic className="size-4" />}
         </div>
       )}
+
+      <AnimatePresence>
+        {showOpenFolder && (
+          <Button
+            size="sm"
+            variant="flat"
+            as={motion.div}
+            onPress={openFolder}
+            // @ts-ignore-next-line
+            transition={{type: 'spring', duration: 0.5}}
+            exit={{opacity: 0, scale: 0.8, translateY: 10}}
+            animate={{opacity: 1, scale: 1, translateY: 0}}
+            initial={{opacity: 0, scale: 0.8, translateY: 10}}
+            className="absolute bottom-2 right-1/2 translate-x-1/2"
+            disableAnimation>
+            <FolderOpen size={16} className="text-foreground-600" />
+          </Button>
+        )}
+      </AnimatePresence>
     </CardFooter>
   );
 });
