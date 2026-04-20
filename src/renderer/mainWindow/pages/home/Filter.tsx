794070e4fbe0f42b8727c47fe491d1df0f0d1366
KindaBrazy <kindofbrazy@gmail.com>
2026-04-20 16:07:42 +0330

    migration(heroui_v3): home filter dropdown

--- src/renderer/mainWindow/pages/home/Filter.tsx
+++ src/renderer/mainWindow/pages/home/Filter.tsx
@@ -1,4 +1,4 @@
-import {Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, Selection} from '@heroui/react';
+import {Dropdown, Label, Selection} from '@heroui-v3/react';
 import {Button} from '@heroui-v3/react';
 import {Apps_Color_Icon, History_Color_Icon, Pin_Color_Icon} from '@lynx_assets/icons/Icons_Colorful';
 import {storageUtilsIpc} from '@lynx_shared/ipc/storage';
@@ -37,42 +37,35 @@ const HomeFilter = memo(({selectedCategories}: HomeFilterProps) => {
   }, []);
 
   return (
-    <Dropdown size="sm" className="border-foreground/15! border drop-shadow-lg" showArrow>
-      <DropdownTrigger>
-        <Button variant="tertiary" className="shrink-0" isIconOnly>
-          <Filter />
-        </Button>
-      </DropdownTrigger>
+    <Dropdown aria-label="Filter Categories">
+      <Button variant="tertiary" className="shrink-0" isIconOnly>
+        <Filter />
+      </Button>
 
-      <DropdownMenu
-        variant="faded"
-        closeOnSelect={false}
-        selectionMode="multiple"
-        aria-label="Filter Categories"
-        selectedKeys={selectedCategories}
-        onSelectionChange={handleSelectionChange}
-        disallowEmptySelection>
-        <DropdownSection title="Categories">
-          <DropdownItem
-            key="Pin"
-            className="cursor-default"
-            startContent={<Pin_Color_Icon className="size-4" id="home_filter_pin" />}>
-            PINNED
-          </DropdownItem>
-          <DropdownItem
-            key="Recently"
-            className="cursor-default"
-            startContent={<History_Color_Icon className="size-4" id="home_filter_history" />}>
-            RECENTLY USED
-          </DropdownItem>
-          <DropdownItem
-            key="All"
-            className="cursor-default"
-            startContent={<Apps_Color_Icon className="size-4" id="home_filter_app_color" />}>
-            ALL
-          </DropdownItem>
-        </DropdownSection>
-      </DropdownMenu>
+      <Dropdown.Popover>
+        <Dropdown.Menu
+          selectionMode="multiple"
+          selectedKeys={selectedCategories}
+          onSelectionChange={handleSelectionChange}>
+          <Dropdown.Section>
+            <Dropdown.Item id="Pin" textValue="Pinned">
+              <Pin_Color_Icon className="size-4" id="home_filter_pin" />
+              <Label>PINNED</Label>
+              <Dropdown.ItemIndicator />
+            </Dropdown.Item>
+            <Dropdown.Item id="Recently" textValue="Recently Used">
+              <History_Color_Icon className="size-4" id="home_filter_history" />
+              <Label>RECENTLY USED</Label>
+              <Dropdown.ItemIndicator />
+            </Dropdown.Item>
+            <Dropdown.Item id="All" textValue="All">
+              <Apps_Color_Icon className="size-4" id="home_filter_app_color" />
+              <Label>ALL</Label>
+              <Dropdown.ItemIndicator />
+            </Dropdown.Item>
+          </Dropdown.Section>
+        </Dropdown.Menu>
+      </Dropdown.Popover>
     </Dropdown>
   );
 });
