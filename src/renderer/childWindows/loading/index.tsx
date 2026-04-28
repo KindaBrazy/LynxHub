cb817c6949b6763663ff288126a75c11f9c29797
KindaBrazy <kindofbrazy@gmail.com>
2026-04-28 08:43:42 +0330

    feat(loading): added `galaxy` animation

--- src/renderer/childWindows/loading/index.tsx
+++ src/renderer/childWindows/loading/index.tsx
@@ -3,6 +3,7 @@ import '@lynx_shared/styles.css';
 import applicationIpc from '@lynx_shared/ipc/application';
 import {createRoot} from 'react-dom/client';
 
+import GalaxyLoading from './loadings/Galaxy';
 import LiquidChromeLoading from './loadings/LiquidChrome';
 import RippleLoading from './loadings/Ripple';
 import SimpleLoading from './loadings/Simple';
@@ -16,7 +17,7 @@ applicationIpc.invoke.disableLoadingAnimations().then(isDisabled => {
   let TargetComponent = SimpleLoading;
 
   if (!isDisabled) {
-    const components = [ThreadsLoading, LiquidChromeLoading, RippleLoading];
+    const components = [ThreadsLoading, LiquidChromeLoading, RippleLoading, GalaxyLoading];
     const randomIndex = Math.floor(Math.random() * components.length);
     TargetComponent = components[randomIndex];
   }
