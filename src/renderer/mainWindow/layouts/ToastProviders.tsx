7109ee6bbf1a0c21fe8af51288f02258cd68cd2e
KindaBrazy <kindofbrazy@gmail.com>
2026-05-23 19:32:10 +0330

    fix(toast): fixed top toast pointer and close button not working
    - added small border

--- src/renderer/mainWindow/layouts/ToastProviders.tsx
+++ src/renderer/mainWindow/layouts/ToastProviders.tsx
@@ -1,4 +1,12 @@
-import {Toast, ToastQueue} from '@heroui/react';
+import {
+  Toast,
+  ToastContent,
+  ToastContentValue,
+  ToastDescription,
+  ToastIndicator,
+  ToastQueue,
+  ToastTitle,
+} from '@heroui/react';
 import createToastFunction, {ToastFunction} from '@lynx_common/utils/toast';
 import {memo} from 'react';
 
@@ -11,8 +19,44 @@ const bottomToast: ToastFunction = createToastFunction(bottomQueue);
 const ToastProviders = memo(() => {
   return (
     <>
-      <Toast.Provider placement="top" queue={topQueue} />
-      <Toast.Provider queue={bottomQueue} placement="bottom end" />
+      <Toast.Provider placement="top" queue={topQueue}>
+        {({toast: toastItem}) => {
+          const content = toastItem.content as ToastContentValue;
+          return (
+            <Toast toast={toastItem} variant={content.variant} className="border notDraggable">
+              <ToastContent>
+                <div className="flex items-center gap-2">
+                  <ToastIndicator variant={content.variant} />
+                  <div className="flex flex-col pr-6">
+                    {content.title ? <ToastTitle>{content.title}</ToastTitle> : null}
+                    {content.description ? <ToastDescription>{content.description}</ToastDescription> : null}
+                  </div>
+                </div>
+              </ToastContent>
+              <Toast.CloseButton className={'notDraggable'} />
+            </Toast>
+          );
+        }}
+      </Toast.Provider>
+      <Toast.Provider queue={bottomQueue} placement="bottom end">
+        {({toast: toastItem}) => {
+          const content = toastItem.content as ToastContentValue;
+          return (
+            <Toast toast={toastItem} className="border" variant={content.variant}>
+              <ToastContent>
+                <div className="flex items-center gap-2">
+                  <ToastIndicator variant={content.variant} />
+                  <div className="flex flex-col pr-6">
+                    {content.title ? <ToastTitle>{content.title}</ToastTitle> : null}
+                    {content.description ? <ToastDescription>{content.description}</ToastDescription> : null}
+                  </div>
+                </div>
+              </ToastContent>
+              <Toast.CloseButton />
+            </Toast>
+          );
+        }}
+      </Toast.Provider>
     </>
   );
 });
