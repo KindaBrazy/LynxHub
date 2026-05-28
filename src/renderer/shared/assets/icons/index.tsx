39a05e90111ec04ca6fb1fea5e1bae960e2bd316
KindaBrazy <kindofbrazy@gmail.com>
2026-05-28 17:56:35 +0330

    feat(icons): added `List` and `Grid` icons

--- src/renderer/shared/assets/icons/index.tsx
+++ src/renderer/shared/assets/icons/index.tsx
@@ -238,3 +238,22 @@ export function MacOS_Icon(props: SvgProps): ReactElement {
     </svg>
   );
 }
+
+export function List_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} fill="none" strokeWidth={2} className="size-4" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+    </svg>
+  );
+}
+export function Grid_Icon(props: SvgProps): ReactElement {
+  return (
+    <svg {...props} fill="none" strokeWidth={2} className="size-4" viewBox="0 0 24 24" stroke="currentColor">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+      />
+    </svg>
+  );
+}
