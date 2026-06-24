76fd25a637a58336c120c9fa3387fb1fb48acce2
KindaBrazy <kindofbrazy@gmail.com>
2026-06-24 17:25:19 +0330

    refactor(icons): replace `SquareTopDown` with `LinkIcon` for action buttons in About section

--- src/renderer/mainWindow/pages/dashboard/content/About.tsx
+++ src/renderer/mainWindow/pages/dashboard/content/About.tsx
@@ -1,4 +1,4 @@
-import {Avatar, Button, Card, Chip} from '@heroui/react';
+import {Avatar, Button, Card, Chip, LinkIcon} from '@heroui/react';
 import SettingsSection from '@lynx/components/SettingsSection';
 import {DiscordColor_Icon, GitHub_Icon, Gmail_Icon, Reddit_Icon, Scales_Icon, XSite_Icon} from '@lynx_assets/icons';
 import {Youtube_Color_Icon} from '@lynx_assets/icons/Icons_Colorful';
@@ -17,7 +17,7 @@ import {
   X_URL,
   YOUTUBE_URL,
 } from '@lynx_common/consts';
-import {Copy, InfoCircle, SquareTopDown} from '@solar-icons/react-perf/BoldDuotone';
+import {Copy, InfoCircle} from '@solar-icons/react-perf/BoldDuotone';
 import {memo, useCallback} from 'react';
 
 import {topToast} from '../../../layouts/ToastProviders';
@@ -42,37 +42,37 @@ const DashboardAbout = memo(() => {
       name: 'GitHub',
       icon: <GitHub_Icon className="size-5" />,
       action: () => window.open(GITHUB_URL),
-      actionIcon: <SquareTopDown className="size-3.5" />,
+      actionIcon: <LinkIcon className="size-3.5" />,
     },
     {
       name: 'Discord',
       icon: <DiscordColor_Icon className="size-5" />,
       action: () => window.open(DISCORD_SERVER),
-      actionIcon: <SquareTopDown className="size-3.5" />,
+      actionIcon: <LinkIcon className="size-3.5" />,
     },
     {
       name: 'X',
       icon: <XSite_Icon className="size-5" />,
       action: () => window.open(X_URL),
-      actionIcon: <SquareTopDown className="size-3.5" />,
+      actionIcon: <LinkIcon className="size-3.5" />,
     },
     {
       name: 'Reddit',
       icon: <Reddit_Icon className="size-5" />,
       action: () => window.open(REDDIT_URL),
-      actionIcon: <SquareTopDown className="size-3.5" />,
+      actionIcon: <LinkIcon className="size-3.5" />,
     },
     {
       name: 'YouTube',
       icon: <Youtube_Color_Icon className="size-5" />,
       action: () => window.open(YOUTUBE_URL),
-      actionIcon: <SquareTopDown className="size-3.5" />,
+      actionIcon: <LinkIcon className="size-3.5" />,
     },
     {
       name: LICENSE_NAME,
       icon: <Scales_Icon className="size-5" />,
       action: () => window.open(LICENSE_PAGE),
-      actionIcon: <SquareTopDown className="size-3.5" />,
+      actionIcon: <LinkIcon className="size-3.5" />,
     },
   ];
 
